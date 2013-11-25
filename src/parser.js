a196c5ea00186e6f889defbb4e1e89f7fd0ae266
Anton Kreuzkamp <akreuzkamp@web.de>
2013-11-25 23:06:59 +0100

    Fix parsing for property-assignment {}.
    An error occured inside the parser when setting a property to {},
    because it was trying to access a non-existing part of the subtree (the content
    of the brackets). This commit adds the needed check if there is any content.

--- src/parser.js
+++ src/parser.js
@@ -1511,7 +1511,7 @@ function QMLBinding(val, tree) {
     // return statement. We decide this on whether it is a code block or not. If it is, we require a
     // return statement. If it is a code block it could though also be a object definition, so we
     // need to check that as well (it is, if the content is labels).
-    this.function = tree && tree[0] == "block" && tree[1][0][0] !== "label";
+    this.function = tree && tree[0] == "block" && tree[1][0] && tree[1][0][0] !== "label";
     this.src = val;
 }
 
