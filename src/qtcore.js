46050fd71074f9f2507e878bb7a96ba7110db3e3
Anton Kreuzkamp <akreuzkamp@web.de>
2013-12-01 18:26:40 +0100

    Add support for all easing types.
    This commit adds support for all easing types supported in QtQuick 1.1.

--- src/qtcore.js
+++ src/qtcore.js
@@ -87,6 +87,19 @@
         Bold: "bold",
         Black: "bolder",
     }
+    var Easing = {
+        Linear: 1,
+        InQuad: 2,          OutQuad: 3,     InOutQuad: 4,           OutInQuad: 5,
+        InCubic: 6,         OutCubic: 7,    InOutCubic: 8,          OutInCubic: 9,
+        InQuart: 10,        OutQuart: 11,   InOutQuart: 12,         OutInQuart: 13,
+        InQuint: 14,        OutQuint: 15,   InOutQuint: 16,         OutInQuint: 17,
+        InSine: 18,         OutSine: 19,    InOutSine: 20,          OutInSine: 21,
+        InExpo: 22,         OutExpo: 23,    InOutExpo: 24,          OutInExpo: 25,
+        InCirc: 26,         OutCirc: 27,    InOutCirc: 28,          OutInCirc: 29,
+        InElastic: 30,      OutElastic: 31, InOutElastic: 32,       OutInElastic: 33,
+        InBack: 34,         OutBack: 35,    InOutBack: 36,          OutInBack: 37,
+        InBounce: 38,       OutBounce: 39,  InOutBounce: 40,        OutInBounce: 41
+    }
     qmlBasicTypes = {
         int: Number,
         real: Number,
@@ -3367,13 +3380,6 @@ function QMLParallelAnimation(meta, parent, engine) {
 function QMLPropertyAnimation(meta, parent, engine) {
     QMLAnimation.call(this, meta, parent, engine);
 
-    // Exports
-    this.Easing = {
-        Linear: 1,
-        InOutCubic: 2
-        // TODO: rest and support for them.
-    };
-
     createSimpleProperty(engine, this, "duration");
     createSimpleProperty(engine, this, "from");
     createSimpleProperty(engine, this, "properties");
@@ -3387,6 +3393,152 @@ function QMLPropertyAnimation(meta, parent, engine) {
     createSimpleProperty(engine, this.easing, "amplitude", { altParent: this });
     createSimpleProperty(engine, this.easing, "overshoot", { altParent: this });
     createSimpleProperty(engine, this.easing, "period", { altParent: this });
+
+    this.easing.$valueForProgress = function(t) {
+        switch(this.type) {
+            // Quad
+            case Easing.InQuad: return Math.pow(t, 2);
+            case Easing.OutQuad: return -Math.pow(t - 1, 2) + 1;
+            case Easing.InOutQuad:
+                if (t < 0.5)
+                    return 2 * Math.pow(t, 2);
+                return -2 * Math.pow(t - 1, 2) + 1;
+            case Easing.OutInQuad:
+                if (t < 0.5)
+                    return -2 * Math.pow(t - 0.5, 2) + 0.5;
+                return 2 * Math.pow(t - 0.5, 2) + 0.5;
+            // Cubic
+            case Easing.InCubic: return Math.pow(t, 3);
+            case Easing.OutCubic: return Math.pow(t - 1, 3) + 1;
+            case Easing.InOutCubic:
+                if (t < 0.5)
+                    return 4 * Math.pow(t, 3);
+                return 4 * Math.pow(t - 1, 3) + 1;
+            case Easing.OutInCubic:
+                return 4 * Math.pow(t - 0.5, 3) + 0.5;
+            // Quart
+            case Easing.InQuart: return Math.pow(t, 4);
+            case Easing.OutQuart: return -Math.pow(t - 1, 4) + 1;
+            case Easing.InOutQuart:
+                if (t < 0.5)
+                    return 8 * Math.pow(t, 4);
+                return -8 * Math.pow(t - 1, 4) + 1;
+            case Easing.OutInQuart:
+                if (t < 0.5)
+                    return -8 * Math.pow(t - 0.5, 4) + 0.5;
+                return 8 * Math.pow(t - 0.5, 4) + 0.5;
+            // Quint
+            case Easing.InQuint: return Math.pow(t, 5);
+            case Easing.OutQuint: return Math.pow(t - 1, 5) + 1;
+            case Easing.InOutQuint:
+                if (t < 0.5)
+                    return 16 * Math.pow(t, 5);
+                return 16 * Math.pow(t - 1, 5) + 1;
+            case Easing.OutInQuint:
+                if (t < 0.5)
+                    return 16 * Math.pow(t - 0.5, 5) + 0.5;
+                return 16 * Math.pow(t - 0.5, 5) + 0.5;
+            // Sine
+            case Easing.InSine: return -Math.cos(0.5 * Math.PI * t) + 1;
+            case Easing.OutSine: return Math.sin(0.5 * Math.PI * t);
+            case Easing.InOutSine: return -0.5 * Math.cos(Math.PI * t) + 0.5;
+            case Easing.OutInSine:
+                if (t < 0.5)
+                    return 0.5 * Math.sin(Math.PI * t);
+                return -0.5 * Math.sin(Math.PI * t) + 1;
+            // Expo
+            case Easing.InExpo: return (1/1023) * (Math.pow(2, 10*t) - 1);
+            case Easing.OutExpo: return -(1024/1023) * (Math.pow(2, -10*t) - 1);
+            case Easing.InOutExpo:
+                if (t < 0.5)
+                    return (1/62) * (Math.pow(2, 10*t) - 1);
+                return -(512/31) * Math.pow(2, -10*t) + (63/62);
+            case Easing.OutInExpo:
+                if (t < 0.5)
+                    return -(16/31) * (Math.pow(2, -10*t) - 1);
+                return (1/1984) * Math.pow(2, 10*t) + (15/31);
+            // Circ
+            case Easing.InCirc: return 1 - Math.sqrt(1 - t*t);
+            case Easing.OutCirc: return Math.sqrt(1 - Math.pow(t - 1, 2));
+            case Easing.InOutCirc:
+                if (t < 0.5)
+                    return 0.5 * (1 - Math.sqrt(1 - 4*t*t));
+                return 0.5 * (Math.sqrt(1 - 4 * Math.pow(t - 1, 2)) + 1);
+            case Easing.OutInCirc:
+                if (t < 0.5)
+                    return 0.5 * Math.sqrt(1 - Math.pow(2 * t - 1, 2));
+                return 0.5 * (2 - Math.sqrt(1 - Math.pow(2 * t - 1, 2)));
+            // Elastic
+            case Easing.InElastic:
+                return -this.amplitude * Math.pow(2, 10 * t - 10)
+                        * Math.sin(2 * t * Math.PI / this.period - Math.asin(1 / this.amplitude));
+            case Easing.OutElastic:
+                return this.amplitude * Math.pow(2, -10 * t)
+                        * Math.sin(2 * t * Math.PI / this.period - Math.asin(1 / this.amplitude))
+                        + 1;
+            case Easing.InOutElastic:
+                if (t < 0.5)
+                    return -0.5 * this.amplitude * Math.pow(2, 20 * t - 10)
+                            * Math.sin(4 * t * Math.PI / this.period - Math.asin(1 / this.amplitude));
+                return -0.5 * this.amplitude * Math.pow(2, -20 * t + 10)
+                        * Math.sin(4 * t * Math.PI / this.period + Math.asin(1 / this.amplitude))
+                        + 1;
+            case Easing.OutInElastic:
+                if (t < 0.5)
+                    return 0.5 * this.amplitude * Math.pow(2, -20 * t)
+                            * Math.sin(4 * t * Math.PI / this.period - Math.asin(1 / this.amplitude))
+                            + 0.5;
+                return -0.5 * this.amplitude * Math.pow(2, 20 * t - 20)
+                        * Math.sin(4 * t * Math.PI / this.period - Math.asin(1 / this.amplitude))
+                        + 0.5;
+            // Back
+            case Easing.InBack: return (this.overshoot + 1) * Math.pow(t, 3) - this.overshoot * Math.pow(t, 2);
+            case Easing.OutBack: return (this.overshoot + 1) * Math.pow(t - 1, 3) + this.overshoot * Math.pow(t - 1, 2) + 1;
+            case Easing.InOutBack:
+                if (t < 0.5)
+                    return 4 * (this.overshoot + 1) * Math.pow(t, 3) - 2 * this.overshoot * Math.pow(t, 2);
+                return 0.5 * (this.overshoot + 1) * Math.pow(2 * t - 2, 3) + this.overshoot/2 * Math.pow(2 * t - 2, 2) + 1;
+            case Easing.OutInBack:
+                if (t < 0.5)
+                    return 0.5 * ((this.overshoot + 1) * Math.pow(2 * t - 1, 3) + this.overshoot * Math.pow(2 * t - 1, 2) + 1);
+                return 4 * (this.overshoot + 1) * Math.pow( t - 0.5, 3) - 2 * this.overshoot * Math.pow(t - 0.5, 2) + 0.5;
+            // Bounce
+            case Easing.InBounce:
+                if (t < 1/11) return -this.amplitude * (121/16) * (t*t - (1/11)*t);
+                if (t < 3/11) return -this.amplitude * (121/16) * (t*t - (4/11)*t + (3/121));
+                if (t < 7/11) return -this.amplitude * (121/16) * (t*t - (10/11)*t + (21/121));
+                return -(121/16) * (t*t - 2*t + 1) + 1;
+            case Easing.OutBounce:
+                if (t < 4/11) return (121/16) * t*t;
+                if (t < 8/11) return this.amplitude * (121/16) * (t*t - (12/11)*t + (32/121)) + 1;
+                if (t < 10/11) return this.amplitude * (121/16) * (t*t - (18/11)*t + (80/121)) + 1;
+                return this.amplitude * (121/16) * (t*t - (21/11)*t + (10/11)) + 1;
+            case Easing.InOutBounce:
+                if (t < 1/22) return -this.amplitude * (121/8) * (t*t - (1/22)*t);
+                if (t < 3/22) return -this.amplitude * (121/8) * (t*t - (2/11)*t + (3/484));
+                if (t < 7/22) return -this.amplitude * (121/8) * (t*t - (5/11)*t + (21/484));
+                if (t < 11/22) return -(121/8) * (t*t - t + 0.25) + 0.5;
+                if (t < 15/22) return (121/8) * (t*t - t) + (137/32);
+                if (t < 19/22) return this.amplitude * (121/8) * (t*t - (17/11)*t + (285/484)) + 1;
+                if (t < 21/22) return this.amplitude * (121/8) * (t*t - (20/11)*t + (399/484)) + 1;
+                return this.amplitude * (121/8) * (t*t - (43/22)*t + (21/22)) + 1;
+            case Easing.OutInBounce:
+                if (t < 4/22) return (121/8) * t*t;
+                if (t < 8/22) return -this.amplitude * (121/8) * (t*t - (6/11)*t + (8/121)) + 0.5;
+                if (t < 10/22) return -this.amplitude * (121/8) * (t*t - (9/11)*t + (20/121)) + 0.5;
+                if (t < 11/22) return -this.amplitude * (121/8) * (t*t - (21/22)*t + (5/22)) + 0.5;
+                if (t < 12/22) return this.amplitude * (121/8) * (t*t - (23/22)*t + (3/11)) + 0.5;
+                if (t < 14/22) return this.amplitude * (121/8) * (t*t - (13/11)*t + (42/121)) + 0.5;
+                if (t < 18/22) return this.amplitude * (121/8) * (t*t - (16/11)*t + (63/121)) + 0.5;
+                return -(121/8) * (t*t - 2*t + (117/121)) + 0.5;
+            // Default
+            default:
+                console.log("Unsupported animation type: ", this.type);
+            // Linear
+            case Easing.Linear:
+                return t;
+        }
+    }
 
     this.$redoActions = function() {
         this.$actions = [];
@@ -3426,7 +3578,10 @@ function QMLPropertyAnimation(meta, parent, engine) {
     }
 
     this.duration = 250;
-    this.easing.type = this.Easing.Linear;
+    this.easing.type = Easing.Linear;
+    this.easing.amplitude = 1;
+    this.easing.period = 0.3;
+    this.easing.overshoot = 1.70158;
     this.$props = [];
     this.$targets = [];
     this.$actions = [];
@@ -3447,19 +3602,6 @@ function QMLNumberAnimation(meta, parent, engine) {
 
     engine.$addTicker(ticker);
 
-    function curve(place) {
-        switch(self.easing.type) {
-
-         case self.Easing.InOutCubic:
-            // todo: better estimate
-            return 0.5 + Math.sin(place*Math.PI - Math.PI / 2) / 2
-         default:
-            console.log("Unsupported animation type: ", self.easing.type);
-         case self.Easing.Linear:
-            return place;
-        }
-    }
-
     function ticker(now, elapsed) {
         if ((self.running || loop === -1) && !self.paused) { // loop === -1 is a marker to just finish this run
             at += elapsed / self.duration;
@@ -3468,7 +3610,7 @@ function QMLNumberAnimation(meta, parent, engine) {
             else
                 for (var i in self.$actions) {
                     var action = self.$actions[i],
-                        value = curve(at) * (action.to - action.from) + action.from;
+                        value = self.easing.$valueForProgress(at) * (action.to - action.from) + action.from;
                     action.target.$properties[action.property].set(value, true);
                 }
         }
