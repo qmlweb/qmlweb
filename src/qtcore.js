cd55b717028cb5918d70d4c530857a0462db4a6a
Anton Kreuzkamp <anton.kreuzkamp@kdab.com>
2014-08-13 15:40:12 +0200

    Do not try to add css-classes to non-Items.

--- src/qtcore.js
+++ src/qtcore.js
@@ -264,7 +264,7 @@ function construct(meta) {
     } else if (component = engine.loadComponent(meta.object.$class)) {
         item = component.createObject(meta.parent);
 
-        if (engine.renderMode == QMLRenderMode.DOM)
+        if (engine.renderMode == QMLRenderMode.DOM && item.dom)
             item.dom.className += " " + meta.object.$class + (meta.object.id ? " " + meta.object.id : "");
     } else {
         console.log("No constructor found for " + meta.object.$class);
