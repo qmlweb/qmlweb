ac5db3a4f376cb81d187a1974457907b53a8a707
Michael Martin Moro <michael@unetresgrossebite.com>
2014-12-22 13:49:27 +0100

    adds an auto-loader for QMLEngine

---
+++ src/qtcore/qml/AutoLoader.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function() {
+  var metaTags = document.getElementsByTagName('BODY');
+
+  for (var i = 0 ; i < metaTags.length ; ++i) {
+    var metaTag = metaTags[i];
+    var source  = metaTag.getAttribute('data-qml');
+
+    if (source != null) {
+      window.qmlEngine = new QMLEngine();
+      qmlEngine.loadFile(source);
+      qmlEngine.start();
+      break ;
+    }
+  }
+});
