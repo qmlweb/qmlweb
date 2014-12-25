0c41d1fae44846252ed931198b3d52d49b65a5aa
Michael Martin Moro <sexymimi64@gmail.com>
2014-12-25 18:41:33 +0000

    fixed qrc not being global

--- src/qtcore/qrc.js
+++ src/qtcore/qrc.js
@@ -1,5 +1,7 @@
-var qrc = {
+(function(global) {
+global.qrc = {
   includesFile: function(path) {
     return typeof qrc[path] != 'undefined';
   }
 };
+}(typeof window != 'undefined' ? global : window);
