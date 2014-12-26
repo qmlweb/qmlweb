44e161ff1d9cf2e4148a640081dadb2fcb704b6f
Michael Martin Moro <sexymimi64@gmail.com>
2014-12-26 19:12:37 +0000

    implemented QtMobility's GeoLocation

---
+++ src/qtcore/qml/elements/QtMobility/GeoLocation.js
@@ -0,0 +1,35 @@
+registerQmlType({
+  module:   'QtMobility',
+  name:     'GeoLocation',
+  versions: /.*/,
+  constructor: function QMLGeoLocation(meta) {
+    var self = this;
+    QMLItem.call(this, meta);
+
+    createSimpleProperty("double", this, "accuracy");
+    createSimpleProperty("double", this, "altitude");
+    createSimpleProperty("double", this, "altitudeAccuracy");
+    createSimpleProperty("double", this, "heading");
+    createSimpleProperty("string", this, "label");
+    createSimpleProperty("double", this, "latitude");
+    createSimpleProperty("double", this, "longitude");
+    createSimpleProperty("double", this, "speed");
+    createSimpleProperty("date",   this, "timestamp");
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition
+    }
+
+    var updatePosition = (function(position) {
+      this.accuracy         = position.coords.accuracy;
+      this.altitude         = position.coords.altitude;
+      this.altitudeAccuracy = position.coords.altitudeAccuracy;
+      this.heading          = position.coords.heading;
+      this.speed            = position.coords.speed;
+      this.timestamp        = position.timestamp;
+    }).bind(this);
+
+    navigator.geolocation.getCurrentPosition(updatePosition);
+    navigator.geolocation.watchPosition(updatePosition);
+  }
+});
