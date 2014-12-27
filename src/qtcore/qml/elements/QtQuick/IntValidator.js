07e72c85738a6b3f1f4f0cddccb20ffd4666a00d
Michael Martin Moro <sexymimi64@gmail.com>
2014-12-27 12:59:15 +0000

    adds QtQuick's IntValidator

---
+++ src/qtcore/qml/elements/QtQuick/IntValidator.js
@@ -0,0 +1,26 @@
+registerQmlType({
+  module:   'QtQuick',
+  name:     'IntValidator',
+  versions: /.*/,
+  constructor: function QMLIntValidator(meta) {
+    QMLItem.call(this, meta);
+
+    createSimpleProperty("int", this, "bottom");
+    createSimpleProperty("int", this, "top");
+    this.bottom = -2147483647;
+    this.top    = 2147483647;
+
+    this.validate = (function(string) {
+      var regExp     = /^(-|\+)?\s*[0-9]+$/;
+      var acceptable = regExp.test(string.trim());
+
+      if (acceptable) {
+        var value    = parseInt(string);
+
+        acceptable   = this.bottom <= value && this.top >= value;
+      }
+      return acceptable;
+    }).bind(this);
+  }
+});
+
