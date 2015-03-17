adea24c6a6d7ac4dff680add706f800bf32d0690
Pavel Vasev <pavel.vasev@gmail.com>
2015-03-17 12:22:34 +0500

    Keep info that import statement uses qualified dotted notation
    The lookup behaviour for dotted-notation vs dir imports is different.

--- src/parser.js
+++ src/parser.js
@@ -1453,7 +1453,9 @@ function qmlparse($TEXT, exigent_mode, embed_tokens) {
             // todo
             next();
             var moduleName = S.token.value;
+            var isDottedNotation = (S.token.type == "name");
             next();
+            
             while (is("punc", ".")) {
                 next();
                 moduleName += "." + S.token.value;
@@ -1469,7 +1471,7 @@ function qmlparse($TEXT, exigent_mode, embed_tokens) {
                 namespace = S.token.value;
                 next();
             }
-            return as("qmlimport", moduleName, version, namespace);
+            return as("qmlimport", moduleName, version, namespace, isDottedNotation);
         }
 
         function qmldocument() {
