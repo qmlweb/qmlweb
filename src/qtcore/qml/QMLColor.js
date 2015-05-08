35b4558baec19a43d14b5f12591e9661e7d5ab75
Pavel Vasev <pavel.vasev@gmail.com>
2015-05-08 11:17:34 +0500

    Fix QMLColor function and Rectangle color handling
    Cannot understand why Rectangle color setters do not pass value via
    QMLColor function by it's own..
    
    PR-URL: https://github.com/qmlweb/qmlweb/pull/123
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>
    Reviewed-By: Igor Andruszkiewicz <igosoft@gmail.com>
    Reviewed-By: Pavel Vasev <pavel.vasev@gmail.com>

--- src/qtcore/qml/QMLColor.js
+++ src/qtcore/qml/QMLColor.js
@@ -1,5 +1,15 @@
 // TODO
 function QMLColor(val) {
-    return val;
-}
-
+  if (typeof val === "number") {
+    // we assume it is int value and must be converted to css hex with padding
+    // http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript
+    val = "#" + (Math.round(val) + 0x1000000).toString(16).substr(-6).toUpperCase();
+  } else {
+    if(typeof val === "array" && val.length >= 3) {
+      // array like [r,g,b] where r,g,b are in 0..1 range
+      var m = 255;
+      val = "rgb(" + Math.round(m * val[0]) + "," + Math.round(m * val[1]) + "," + Math.round(m * val[2]) + ")";
+    }
+  }
+  return val;
+};
