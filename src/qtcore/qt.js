15e50a70345e1d8f340ca30353cd7eccc7253fcf
Arno Pähler <arno@arnopaehler.de>
2015-07-22 17:43:39 +0200

    Add Qt.Align* constants
    PR-URL: https://github.com/qmlweb/qmlweb/pull/167
    Reviewed-By: Michael Martin Moro <michael@unetresgrossebite.com>
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- src/qtcore/qt.js
+++ src/qtcore/qt.js
@@ -269,6 +269,17 @@ global.Qt = {
   Key_Sleep: 95,
   Key_Zoom: 251,
   Key_Cancel: 3,
+
+  AlignLeft: 0x0001,
+  AlignRight: 0x0002,
+  AlignHCenter: 0x0004,
+  AlignJustify: 0x0008,
+
+  AlignTop: 0x0020,
+  AlignBottom: 0x0040,
+  AlignVCenter: 0x0080,
+  AlignBaseline: 0x0100,
+  AlignCenter: 0x0084,
   // Screen
   PrimaryOrientation: 0,
   PortraitOrientation: 1,
