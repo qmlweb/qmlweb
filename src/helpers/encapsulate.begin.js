f33d663710690a78900b014ea2f437c56a31bedf
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-02-14 22:48:36 +0300

    Drop ie8 polyfill.
    It's 2016 on my calendar.

--- src/helpers/encapsulate.begin.js
+++ src/helpers/encapsulate.begin.js
@@ -1,3 +1,1 @@
 (function(global){
-  // Polyfills
-  Function.prototype.bind=(function(){}).bind||function(b){if(typeof this!=="function"){throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");}function c(){}var a=[].slice,f=a.call(arguments,1),e=this,d=function(){return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));};c.prototype=this.prototype;d.prototype=new c();return d;};
