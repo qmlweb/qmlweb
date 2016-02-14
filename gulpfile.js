3e09459e77d5f8bf8bed0bfff11919705a64c9d0
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-02-14 14:33:39 +0300

    gulpfile: Order files before concatenating, fixes #10.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
 var gulp    = require('gulp');
+var order   = require('gulp-order');
 var uglify  = require('gulp-uglify');
 var concat  = require('gulp-concat');
 var rename  = require('gulp-rename');
 var jasmine = require('gulp-jasmine-phantom');
 
 var qtcoreSources = [
-  './src/helpers/encapsulate.begin.js',
-  './src/qtcore/qml/QMLBinding.js',
+  'src/helpers/encapsulate.begin.js',
+  'src/qtcore/qml/QMLBinding.js',
 
-  './src/uglify/ast.js',
-  './src/uglify/parse.js',
-  './src/uglify/utils.js',
+  'src/uglify/ast.js',
+  'src/uglify/parse.js',
+  'src/uglify/utils.js',
 
-  './src/qtcore/qml/lib/parser.js',
-  './src/qtcore/qml/lib/process.js',
-  './src/qtcore/qml/lib/import.js',
+  'src/qtcore/qml/lib/parser.js',
+  'src/qtcore/qml/lib/process.js',
+  'src/qtcore/qml/lib/import.js',
 
-  './src/qtcore/qrc.js',
-  './src/qtcore/qt.js',
-  './src/qtcore/signal.js',
-  './src/qtcore/font.js',
-  './src/qtcore/easing.js',
-  './src/qtcore/qml/qml.js',
-  './src/qtcore/qml/**/*.js',
+  'src/qtcore/qrc.js',
+  'src/qtcore/qt.js',
+  'src/qtcore/signal.js',
+  'src/qtcore/font.js',
+  'src/qtcore/easing.js',
+  'src/qtcore/qml/qml.js',
+  'src/qtcore/qml/**/*.js',
 
-  './src/qmlweb/**/*.js',
-  './src/helpers/encapsulate.end.js'
+  'src/qmlweb/**/*.js',
+  'src/helpers/encapsulate.end.js'
 ];
 
 var tests = [
-  './lib/qt.js',
-  './spec/**/*.js'
+  'lib/qt.js',
+  'spec/**/*.js'
 ];
 
 gulp.task('qt', function() {
   return gulp.src(qtcoreSources)
+             .pipe(order(qtcoreSources))
              .pipe(concat('qt.js'))
              .pipe(gulp.dest('./lib'));
 });
