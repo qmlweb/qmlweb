e399a2beef571f4fbf47209222d30b899c5f1c01
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-02-15 16:41:19 +0300

    Initial WebSocket implementation, closes #13.

---
+++ src/qtcore/qml/elements/QtWebSockets/WebSocket.js
@@ -0,0 +1,81 @@
+// WARNING: Can have wrong behavior if url is changed while the socket is in Connecting state.
+// TODO: Recheck everything.
+
+registerQmlType({
+  module:   'QtWebSockets',
+  name:     'WebSocket',
+  versions: /.*/,
+  constructor: function QMLWebSocket(meta) {
+    QMLBaseObject.call(this, meta);
+
+    // Exports.
+    this.WebSocket = {
+        // status
+        Connecting: 0,
+        Open: 1,
+        Closing: 2,
+        Closed: 3,
+        Error: 4
+    }
+
+    createSimpleProperty("bool", this, "active");
+    createSimpleProperty("enum", this, "status");
+    createSimpleProperty("string", this, "errorString");
+    createSimpleProperty("url", this, "url");
+
+    this.textMessageReceived = Signal([{type: "string", name: "message"}]);
+
+    this.status = this.WebSocket.Closed;
+
+    var self = this,
+        socket,
+        reconnect = false;
+
+    this.sendTextMessage = function(message) {
+        if (this.status == this.WebSocket.Open)
+            socket.send(message);
+    }
+
+    function connectSocket() {
+        reconnect = false;
+
+        if (!self.url || !self.active)
+            return;
+
+        self.status = self.WebSocket.Connecting;
+        socket = new WebSocket(self.url);
+        socket.onopen = function() {
+            self.status = self.WebSocket.Open;
+        }
+        socket.onclose = function() {
+            self.status = self.WebSocket.Closed;
+            if (reconnect)
+                connectSocket();
+        }
+        socket.onerror = function(error) {
+            self.errorString = error.message;
+            self.status = self.WebSocket.Error;
+        }
+        socket.onmessage = function(message) {
+            self.textMessageReceived(message.data);
+        }
+    };
+
+    function reconnectSocket() {
+        reconnect = true;
+        if (self.status == self.WebSocket.Open) {
+            self.status = self.WebSocket.Closing;
+            socket.close()
+        } else if (self.status != self.WebSocket.Closing) {
+            connectSocket();
+        }
+    };
+
+    this.statusChanged.connect(this, function(status) {
+        if (status != self.WebSocket.Error)
+            self.errorString = "";
+    });
+    this.activeChanged.connect(this, reconnectSocket);
+    this.urlChanged.connect(this, reconnectSocket);
+  }
+});
