6b7593a86131971c1fb9c806b02ecc98271141d9
Igor Andruszkiewicz <igosoft@gmail.com>
2016-02-21 17:10:11 +0100

    Update Loader

--- src/qtcore/qml/elements/QtQuick/Loader.js
+++ src/qtcore/qml/elements/QtQuick/Loader.js
@@ -16,7 +16,7 @@ registerQmlType({
     name: 'Loader',
     versions: /.*/,
     baseClass: QMLItem,
-    constructor: function(meta) {
+    constructor: function QMLLoader(meta) {
         QMLItem.call(this, meta);
 
         var self = this;
@@ -36,23 +36,124 @@ registerQmlType({
         this.source = undefined;
         this.sourceComponent = undefined;
         this.status = 1;
+        this.sourceUrl = '';
 
         this.loaded = Signal();
 
-        this.qml = 'not set';
-        this.sourceUrl = 'empty';
-        this.sourceQml = 'empty';
+        this.activeChanged.connect( function(newVal) {
+            if (self.active){
+                if (self.source)
+                    sourceChanged();
+                else if (self.sourceComponent)
+                    sourceComponentChanged();
+            }else{
+                unload();
+            }
+         });
+
+        this.sourceChanged.connect( function(newVal) {
+            if (self.active == false )//|| (newVal == self.sourceUrl && self.item !== undefined) ) //todo
+            {
+                console.log( " Loader isn't active.");
+                return;
+            }
+
+            unload();
+
+            if (self.source.length>0) {
+               var fileName = newVal.lastIndexOf(".qml") == newVal.length-4 ? newVal.substring(0, newVal.length-4) : "";
+
+               if ( fileName !== "" ) {
+                   var tree = engine.loadComponent(fileName);
+                   var meta = { object: tree, context: self , parent: self};
+
+                   var qmlComponent = new QMLComponent(meta);
+                   var loadedComponent = createComponentObject(qmlComponent, self);
+
+                   self.sourceComponent = loadedComponent;
+                   self.sourceUrl = newVal;
+               }
+            }
+        } );
+
+        this.sourceComponentChanged.connect(function(newItem) {
+            if ( self.active == false ) {
+                return;
+            }
+
+            unload();
+
+            var qmlComponent = newItem;
+
+            if (newItem instanceof QMLComponent) {
+                  var meta = { object: newItem.$metaObject, context: self , parent: self};
+                  qmlComponent = construct(meta);
+            }
+
+            qmlComponent.parent = self;
+            self.item = qmlComponent;
+
+            updateGeometry();
+
+            if (self.item){
+                self.loaded();
+            }
+        } );
+
+        this.widthChanged.connect(function(newWidth) {updateGeometry();} );
+        this.heightChanged.connect(function(newHeight) {updateGeometry();} );
+
+        function createComponentObject(qmlComponent, parent){
+            var newComponent = qmlComponent.createObject(parent);
+
+            newComponent.parent = parent;
+            qmlComponent.finalizeImports();
+
+             if (engine.operationState !== QMLOperationState.Init) {
+             //   We don't call those on first creation, as they will be called
+             //   by the regular creation-procedures at the right time.
+                engine.$initializePropertyBindings();
+                callOnCompleted(newComponent);
+             }
+
+            return newComponent;
+        }
+
+        function updateGeometry(){
+            // Loader size doesn't exist
+            if (!self.width) {
+                self.width = self.item ? self.item.width : 0;
+            } else {
+                // Loader size exists
+                if (self.item) self.item.width = self.width;
+            }
+
+            if (!self.height) {
+                self.height = self.item ? self.item.height : 0;
+            } else {
+                // Loader size exists
+                 if (self.item) self.item.height = self.height;
+            }
+        }
+
+        function unload(){
+          if (self.item){
+            self.item.$delete();
+            self.item.parent = undefined;
+            self.item = undefined;
+          }
+        }
+
+        function callOnCompleted(child) {
+            child.Component.completed();
+            for (var i = 0; i < child.children.length; i++)
+                callOnCompleted(child.children[i]);
+        }
 
         this.setSource = function(url, options) {
             this.sourceUrl = url;
-            this.sourceQml = getUrlContents(url);
-
             this.props = options;
             this.source = url;
-            this.sourceComponent = null; // TODO Component
-            this.item = {}; // TODO
-            this.loaded();
         }
-
     }
 });
