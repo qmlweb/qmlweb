e94f9c588841080bd6282e0e08f11b481af39e60
henrikrudstrom <hrudstrom@gmail.com>
2016-02-21 19:43:22 +0100

    tests: Basic property types and alias tests
    PR-URL: https://github.com/qmlweb/qmlweb/pull/63
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

---
+++ tests/QMLEngine/properties.js
@@ -0,0 +1,16 @@
+describe('properties', function() {
+  var loader = prefixedQmlLoader('QMLEngine/qml/Properties');
+  it('can store values', function() {
+    var qml = loader('Basic').qml;
+    expect(qml.intProperty).toBe(10);
+    expect(qml.doubleProperty).toBe(0.5);
+    expect(qml.stringProperty).toBe("hello");
+    expect(qml.itemProperty.x).not.toBe(undefined);
+    expect(qml.arrayProperty).toEqual([1, 2, "bar"]);
+  });
+
+  it('can be aliased', function() {
+    var qml = loader('Alias').qml;
+    expect(qml.childX).toBe(125);
+  });
+});
---
+++ tests/QMLEngine/qml/PropertiesBasic.qml
@@ -0,0 +1,7 @@
+Item {
+  property int intProperty: 10
+  property double doubleProperty: 0.5
+  property string stringProperty: "hello"
+  property Item itemProperty: Item { }
+  property var arrayProperty: [1, 2, "bar"]
+}
