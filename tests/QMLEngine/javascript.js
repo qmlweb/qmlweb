be0df14239d0b550d2882b12c873514221342971
Michael Martin Moro <michael@unetresgrossebite.com>
2016-02-25 11:12:36 +0100

    adds a test to check that the js parser supports regexps
    PR-URL: https://github.com/qmlweb/qmlweb/pull/74
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- tests/QMLEngine/javascript.js
+++ tests/QMLEngine/javascript.js
@@ -4,4 +4,12 @@ describe('QMLEngine.javascript', function() {
     var div = loader('BasicSyntax');
     div.remove()
   });
+
+  it('can parse regexp', function() {
+    var qml = loader('Regexp').qml;
+    expect("toto".match(qml.reg)).toBe(null);
+    expect("4242/firstsecond".match(qml.reg)).not.toBe(null);
+    qml.dom.remove();
+  });
 });
+
---
+++ tests/QMLEngine/qml/JavascriptRegexp.qml
@@ -0,0 +1,3 @@
+Item {
+  property variant reg: /^[0-9]+\/(first|second){1,2}$/
+}
