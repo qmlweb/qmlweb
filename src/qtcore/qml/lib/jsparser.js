31aac69c74c5195823fbce13a8905ca308a31df6
henrikrudstrom <hrudstrom@gmail.com>
2016-03-11 13:08:58 +0000

    replace js parsing with `eval()`  and remove uglify sources

--- src/qtcore/qml/lib/jsparser.js
+++ src/qtcore/qml/lib/jsparser.js
@@ -6,33 +6,16 @@
   else
     uglify_parse = parse;
 
+
   global.importJavascriptInContext = function (jsData, $context) {
-    with($context) {
-      eval(jsData.source);
-      for (var i = 0 ; i < jsData.exports.length ; ++i) {
-        var symbolName = jsData.exports[i];
-        $context[symbolName] = eval(symbolName);
-      }
+    var imported = eval('('+jsData.source+')');
+    for (var attrname in imported) { 
+      $context[attrname] = imported[attrname]; 
     }
   }
 
   global.jsparse = function (source) {
-    var AST_Tree = uglify_parse(source);
-    var obj = { exports: [], source: source };
-
-    for (var i = 0 ; i < AST_Tree.body.length ; ++i) {
-      var element = AST_Tree.body[i];
-
-      switch (element.__proto__.TYPE) {
-        case "VAR":
-          obj.exports.push(element.definitions[0].name.name);
-          break ;
-        case "Defun":
-          obj.exports.push(element.name.name);
-          break ;
-      }
-    }
-    return obj;
+    return { exports: [], source: source };
   }
 
 })();
