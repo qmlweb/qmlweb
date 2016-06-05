5d37aa8f24836506f7a09af8f8a8f5c62689aeaa
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-06-05 21:28:45 +0300

    Checkbox: store drawn DOM element in this.impl
    This also fixes positioning issues.
    
    Refs: https://github.com/qmlweb/qmlweb/issues/234

--- src/modules/QtQuick.Controls/Checkbox.js
+++ src/modules/QtQuick.Controls/Checkbox.js
@@ -5,38 +5,47 @@ registerQmlType({
   baseClass: 'QtQuick.Item',
   constructor: function QMLCheckbox(meta) {
     callSuper(this, meta);
-    this.dom = document.createElement("label");
+
+    const label = this.impl = document.createElement('label');
+    label.style.pointerEvents = 'auto';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.style.verticalAlign = 'text-bottom';
+    label.appendChild(checkbox);
+
+    const span = document.createElement('span');
+    label.appendChild(span);
+
+    this.dom.appendChild(label);
+
     var self = this;
 
     var QMLFont = new getConstructor('QtQuick', '2.0', 'Font');
     this.font = new QMLFont(this);
-
-    this.dom.innerHTML = "<input type=\"checkbox\"><span></span>";
-    this.dom.style.pointerEvents = "auto";
-    this.dom.firstChild.style.verticalAlign = "text-bottom";
 
     createProperty("string", this, "text");
     createProperty("bool", this, "checked");
     createProperty("color", this, "color");
 
     this.Component.completed.connect(this, function() {
-        this.implicitHeight = this.dom.offsetHeight;
-        this.implicitWidth = this.dom.offsetWidth > 0 ? this.dom.offsetWidth + 4 : 0;
+        this.implicitHeight = label.offsetHeight;
+        this.implicitWidth = label.offsetWidth > 0 ? label.offsetWidth + 4 : 0;
     });
     this.textChanged.connect(this, function(newVal) {
-        this.dom.children[1].innerHTML = newVal;
-        this.implicitHeight = this.dom.offsetHeight;
-        this.implicitWidth = this.dom.offsetWidth > 0 ? this.dom.offsetWidth + 4 : 0;
+        span.innerHTML = newVal;
+        this.implicitHeight = label.offsetHeight;
+        this.implicitWidth = label.offsetWidth > 0 ? label.offsetWidth + 4 : 0;
     });
     this.colorChanged.connect(this, function(newVal) {
-        this.dom.children[1].style.color = QMLColor(newVal);
+        span.style.color = QMLColor(newVal);
     });
 
     this.checkedChanged.connect(this, function(newVal) {
-        this.dom.firstChild.checked = self.checked;
+        checkbox.checked = self.checked;
     });
 
-    this.dom.firstChild.onchange = function() {
+    checkbox.onchange = function() {
         self.checked = this.checked;
     };
   }
