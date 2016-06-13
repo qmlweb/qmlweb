a5f7bd2df0817b3d5e96bb27926863cd8709bf3b
Igor Andruszkiewicz <igosoft@gmail.com>
2016-06-13 22:18:33 +0200

    Rectange: various fixes for border sizes
    PR-URL: https://github.com/qmlweb/qmlweb/pull/114
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- src/modules/QtQuick/Rectangle.js
+++ src/modules/QtQuick/Rectangle.js
@@ -3,7 +3,10 @@ registerQmlType({
   name:   'Rectangle',
   versions: /.*/,
   baseClass: 'Item',
-  constructor: function QMLRectangle(meta) {
+  constructor: QMLRectangle
+});
+
+function QMLRectangle(meta) {
     callSuper(this, meta);
 
     createProperty("color", this, "color", {initialValue: 'white'});
@@ -31,10 +34,79 @@ registerQmlType({
     });
     this.border.colorChanged.connect(this, function(newVal) {
         bg.style.borderColor = QMLColor(newVal);
-        bg.style.borderWidth = this.border.width + 'px';
+        if (bg.style.borderWidth == '0px') {
+            bg.style.borderWidth = this.border.width + 'px';
+        }
+        this.$updateBorder(this.border.width);
     });
     this.border.widthChanged.connect(this, function(newVal) {
-        bg.style.borderWidth = newVal + 'px';
+        // ignore negative border width
+        if (newVal >= 0) {
+            this.$updateBorder(newVal);
+        } else {
+            bg.style.borderWidth = "0px";
+        }
     });
-  }
-});
+    this.widthChanged.connect(this, function(newVal){
+       this.$updateBorder(this.border.width);
+    });
+    this.heightChanged.connect(this, function(newVal){
+       this.$updateBorder(this.border.width);
+    });
+}
+
+QMLRectangle.prototype.$updateBorder = function(newBorderWidth) {
+    const bg = this.dom.firstChild;
+
+    // ignore negative and 0px border width
+    if (newBorderWidth == "0px" || newBorderWidth < 0) {
+        return;
+    }
+    // no Rectangle border width was set yet
+    if ( (newBorderWidth == "1" && bg.style.borderWidth == "0px") || ( typeof newBorderWidth === "undefined" && bg.style.borderWidth == "0px") ) {
+        return;
+    }
+
+    var topBottom = typeof newBorderWidth === "undefined" ? bg.style.borderWidth : newBorderWidth + 'px';
+    var leftRight = topBottom;
+
+    bg.style.borderTopWidth = topBottom;
+    bg.style.borderBottomWidth = topBottom;
+    bg.style.borderLeftWidth = leftRight;
+    bg.style.borderRightWidth = leftRight;
+
+    // hide border if any of dimensions is less then one
+    if (this.width <= 0 || this.height <= 0 || typeof this.width === "undefined" || typeof this.height === "undefined") {
+        bg.style.borderWidth = '0px';
+    } else {
+        // check if border is not greater than Rectangle size
+        // react by change of width or height of div (in css)
+
+        if (2 * this.border.width > this.height) {
+            topBottom = this.height/2 + 'px';
+            bg.style.height = '0px';
+        } else {
+            if ( this.height - 2 * this.border.width < this.border.width) {
+                if (this.height > 2) {
+                    bg.style.height = (this.height%2 ? -1 : -2 + this.height + (this.height - (2*this.border.width))) + 'px';
+                }
+            }
+        }
+
+        if (2 * this.border.width > this.width) {
+            leftRight = this.width/2 + 'px';
+            bg.style.width = '0px';
+        } else {
+            if (this.width - 2 * this.border.width < this.border.width) {
+                if (this.width > 2) {
+                    bg.style.width = (this.width%2 ? -1 : -2 + this.width + ( this.width - (2*this.border.width))) + 'px';
+                }
+            }
+        }
+
+        bg.style.borderTopWidth = topBottom;
+        bg.style.borderBottomWidth = topBottom;
+        bg.style.borderLeftWidth = leftRight;
+        bg.style.borderRightWidth = leftRight;
+    }
+};
--- tests/Render/Simple/RectangleBorder.qml
+++ tests/Render/Simple/RectangleBorder.qml
@@ -2,27 +2,26 @@ import QtQuick 2.5
 
 Rectangle {
   width: 75
-  height: 15
+  height: 21
   color: 'green'
 
   Rectangle {
-    width: 10
+    width: 5
     height: 10
   }
   Rectangle {
     width: 11
     height: 10
     x: 15
-    border.color: 'red'
+    border.color: "#27c1cf"
   }
   Rectangle {
     width: 10
-    height: 12
+    height: 10
     x: 30
     border.width: 2
   }
   Rectangle {
-    // no width/heigth is set but border is set
     x: 45
     color: 'orange'
     border.width: 1
@@ -31,6 +30,39 @@ Rectangle {
     x: 60
     border.width: 0
     border.color: 'red'
+    width: 10
+    height: 10
+  }
+  Rectangle {
+    x: 0
+    y: 11
+    border.width: 20
+    border.color: 'red'
+    width: 10
+    height: 10
+  }
+  Rectangle {
+    x: 15
+    y: 11
+    border.width: -20
+    border.color: 'pink'
+    width: 10
+    height: 10
+  }
+  Rectangle {
+    x: 30
+    y: 11
+    border.width: 3
+    border.color: 'red'
+    width: -10
+    height: 10
+  }
+  Rectangle {
+    x: 45
+    y: 11
+    color: 'red'
+    border.width: 3
+    border.color: 'transparent'
     width: 10
     height: 10
   }
