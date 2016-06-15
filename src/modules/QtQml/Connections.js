2bc35bb6bde2d201fa9e471208fdfa63794c5939
Stephen D'Angelo <stephen.m.dangelo@gmail.com>
2016-06-15 11:21:32 -0400

    QtQml: Implemented Connections

---
+++ src/modules/QtQml/Connections.js
@@ -0,0 +1,34 @@
+registerQmlType({
+  module: 'QtQml',
+  name: 'Connections',
+  versions: /.*/,
+  baseClass: 'QtObject',
+  properties: {
+    target: "QtObject",
+    ignoreUnknownSignals: "bool"
+  }
+}, class {
+  constructor(meta) {
+    callSuper(this, meta);
+    this.target = this.$parent
+    this.$connections = {}
+
+    let old_target = this.target;
+    const reconnectTarget = () => {
+      for (var i in this.$connections) {
+        var c = this.$connections[i];
+        if (c._currentConnection && old_target && old_target[i] && typeof old_target[i].disconnect === 'function') {
+          old_target[i].disconnect(c._currentConnection);
+        }
+        c._currentConnection = connectSignal(this.target, i, c.value, c.objectScope, c.componentScope);
+      }
+      old_target = this.target;
+    };
+
+    this.targetChanged.connect(reconnectTarget);
+    this.Component.completed.connect(reconnectTarget);
+  }
+  $setCustomSlot(propName, value, objectScope, componentScope) {
+    this.$connections[propName] = { value, objectScope, componentScope };
+  }
+});
---
+++ tests/QtQml/Connections.js
@@ -0,0 +1,14 @@
+describe('QtQml.Connections', function() {
+  setupDivElement();
+  var load = prefixedQmlLoader('QtQml/qml/Connections');
+
+  it('Connections', function() {
+    var qml = load('Connections', this.div);
+    expect(qml.connections.target).toBe(qml);
+    qml.value = 1;
+    expect(qml.test_value).toBe(1);
+    qml.connections.target = qml.new_target;
+    qml.new_target.value = 1;
+    expect(qml.test_value).toBe(2);
+  });
+});
