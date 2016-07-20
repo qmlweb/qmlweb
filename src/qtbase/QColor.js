7fac5935402a13c63e3b5719c20f49cd32fe472a
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-07-20 08:26:42 +0300

    Revert "QColor: use WeakMap to store colors"
    This reverts commit f64ebbc148df28f23a6e18621ff987c3342e8065.
    
    We can't use string keys with WeakMap.

--- src/qtbase/QColor.js
+++ src/qtbase/QColor.js
@@ -19,13 +19,13 @@ class QColor {
   }
   $get() {
     // Returns the same instance for all equivalent colors.
-    // NOTE: the returned value should not be changed using method calls, if
-    // those would be added in the future, the returned value should be wrapped.
-    if (!QColor.colors.has(this.$value)) {
-      QColor.colors.set(this.$value, this);
+    // Note that we can't return this instance, as it could be changed later.
+    // TODO: use a WeakMap with a polyfill to reduce the potential memory hit.
+    if (!QColor.colors[this.$value]) {
+      QColor.colors[this.$value] = new QColor(this.$value);
     }
-    return QColor.colors.get(this.$value);
+    return QColor.colors[this.$value];
   }
 }
-QColor.colors = new WeakMap();
+QColor.colors = {};
 QmlWeb.QColor = QColor;
