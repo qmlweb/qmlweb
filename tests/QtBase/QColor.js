74c73a7810a97d416bf14ac12d918ebdc26efaf5
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-07-20 20:31:59 +0300

    tests: Add QColor construction tests

--- tests/QtBase/QColor.js
+++ tests/QtBase/QColor.js
@@ -3,6 +3,18 @@ describe("QtBase.QColor", function() {
     expect(!!QmlWeb && !!QmlWeb.QColor).toBe(true);
   });
 
+  it("construction", function() {
+    var colors = [
+      "#abcDEF",
+      "#abcdef",
+      0xabcdef
+    ];
+    colors.forEach(function(input) {
+      var color = new QmlWeb.QColor(input);
+      expect(color.toString()).toBe("#abcdef");
+    });
+  });
+
   it("comparison", function() {
     var color = new QmlWeb.QColor("#abcDEF");
     expect(color.toString()).toBe("#abcdef");
