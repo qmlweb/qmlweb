6f746cb5656e942ec94397fcf1b22df27354a8ca
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-07-31 11:12:49 +0300

    QtQuick: clean up Loader

--- src/modules/QtQuick/Loader.js
+++ src/modules/QtQuick/Loader.js
@@ -16,126 +16,103 @@ registerQmlType({
     loaded: []
   },
 }, class {
-    constructor(meta) {
-        callSuper(this, meta);
+  constructor(meta) {
+    callSuper(this, meta);
 
-        let sourceUrl = '';
+    this.$sourceUrl = "";
 
-        const QMLComponent = QmlWeb.getConstructor('QtQml', '2.0', 'Component');
-
-        this.activeChanged.connect(() => {
-            if (!this.active) {
-                unload();
-                return;
-            }
-            if (this.source) {
-                sourceChanged();
-            } else if (this.sourceComponent) {
-                sourceComponentChanged();
-            }
-        });
-
-        this.sourceChanged.connect(newVal => {
-            // if (newVal == sourceUrl && this.item !== undefined) return // TODO
-            if (!this.active) {
-                return;
-            }
-
-            unload();
-
-            // TODO: we require '.qml' for now, that should be fixed
-            if (newVal.length <= 4) { // 0
-                return;
-            }
-            if (newVal.substr(newVal.length - 4, 4) !== '.qml') {
-                return;
-            }
-            var fileName = newVal.substring(0, newVal.length - 4);
-
-            var tree = QmlWeb.engine.loadComponent(fileName);
-            var meta = { object: tree, context: this, parent: this };
-            var qmlComponent = new QMLComponent(meta);
-            var loadedComponent = createComponentObject(qmlComponent, this);
-            this.sourceComponent = loadedComponent;
-            sourceUrl = newVal;
-        });
-
-        this.sourceComponentChanged.connect(newItem => {
-            if (!this.active) {
-                return;
-            }
-
-            unload();
-
-            var qmlComponent = newItem;
-
-            if (newItem instanceof QMLComponent) {
-                  var meta = { object: newItem.$metaObject, context: this, parent: this };
-                  qmlComponent = QmlWeb.construct(meta);
-            }
-
-            qmlComponent.parent = this;
-            this.item = qmlComponent;
-
-            updateGeometry();
-
-            if (this.item) {
-                this.loaded();
-            }
-        });
-
-        function createComponentObject(qmlComponent, parent) {
-            var newComponent = qmlComponent.createObject(parent);
-
-            newComponent.parent = parent;
-            qmlComponent.finalizeImports();
-
-             if (QmlWeb.engine.operationState !== QmlWeb.QMLOperationState.Init) {
-                // We don't call those on first creation, as they will be called
-                // by the regular creation-procedures at the right time.
-                QmlWeb.engine.$initializePropertyBindings();
-                callOnCompleted(newComponent);
-             }
-
-            return newComponent;
-        }
-
-        const updateGeometry = () => {
-            // Loader size doesn't exist
-            if (!this.width) {
-                this.width = this.item ? this.item.width : 0;
-            } else if (this.item) {
-                // Loader size exists
-                this.item.width = this.width;
-            }
-
-            if (!this.height) {
-                this.height = this.item ? this.item.height : 0;
-            } else if (this.item) {
-                // Loader size exists
-                this.item.height = this.height;
-            }
-        }
-        this.widthChanged.connect(updateGeometry);
-        this.heightChanged.connect(updateGeometry);
-
-        const unload = () => {
-          if (this.item) {
-            this.item.$delete();
-            this.item.parent = undefined;
-            this.item = undefined;
-          }
-        }
-
-        function callOnCompleted(child) {
-            child.Component.completed();
-            child.children.forEach(callOnCompleted);
-        }
-
-        this.setSource = function(url, options) {
-            sourceUrl = url;
-            this.props = options;
-            this.source = url;
-        }
+    this.activeChanged.connect(this, this.$onActiveChanged);
+    this.sourceChanged.connect(this, this.$onSourceChanged);
+    this.sourceComponentChanged.connect(this, this.$onSourceComponentChanged);
+    this.widthChanged.connect(this, this.$updateGeometry);
+    this.heightChanged.connect(this, this.$updateGeometry);
+  }
+  $onActiveChanged() {
+    if (!this.active) {
+      this.$unload();
+      return;
     }
+    if (this.source) {
+      this.$onSourceChanged(this.source);
+    } else if (this.sourceComponent) {
+      this.$onSourceComponentChanged(this.sourceComponent);
+    }
+  }
+  $onSourceChanged(newVal) {
+    // if (newVal == this.$sourceUrl && this.item !== undefined) return; // TODO
+    if (!this.active) return;
+    this.$unload();
+
+    // TODO: we require ".qml" for now, that should be fixed
+    if (newVal.length <= 4) return;
+    if (newVal.substr(newVal.length - 4, 4) !== ".qml") return;
+    const fileName = newVal.substring(0, newVal.length - 4);
+
+    const tree = QmlWeb.engine.loadComponent(fileName);
+    const QMLComponent = QmlWeb.getConstructor("QtQml", "2.0", "Component");
+    const meta = { object: tree, context: this, parent: this };
+    const qmlComponent = new QMLComponent(meta);
+    const loadedComponent = this.$createComponentObject(qmlComponent, this);
+    this.sourceComponent = loadedComponent;
+    this.$sourceUrl = newVal;
+  }
+  $onSourceComponentChanged(newItem) {
+    if (!this.active) return;
+    this.$unload();
+    const QMLComponent = QmlWeb.getConstructor("QtQml", "2.0", "Component");
+    let qmlComponent = newItem;
+    if (newItem instanceof QMLComponent) {
+      const meta = { object: newItem.$metaObject, context: this, parent: this };
+      qmlComponent = QmlWeb.construct(meta);
+    }
+    qmlComponent.parent = this;
+    this.item = qmlComponent;
+    this.$updateGeometry();
+    if (this.item) {
+      this.loaded();
+    }
+  }
+  setSource(url, options) {
+    this.$sourceUrl = url;
+    this.props = options;
+    this.source = url;
+  }
+  $unload() {
+    if (!this.item) return;
+    this.item.$delete();
+    this.item.parent = undefined;
+    this.item = undefined;
+  }
+  static callOnCompleted(child) {
+    child.Component.completed();
+    child.children.forEach(this.callOnCompleted);
+  }
+  $createComponentObject(qmlComponent, parent) {
+    const newComponent = qmlComponent.createObject(parent);
+    newComponent.parent = parent;
+    qmlComponent.finalizeImports();
+    if (QmlWeb.engine.operationState !== QmlWeb.QMLOperationState.Init) {
+      // We don't call those on first creation, as they will be called
+      // by the regular creation-procedures at the right time.
+      QmlWeb.engine.$initializePropertyBindings();
+      this.constructor.callOnCompleted(newComponent); // call static
+    }
+    return newComponent;
+  }
+  $updateGeometry() {
+    // Loader size doesn't exist
+    if (!this.width) {
+      this.width = this.item ? this.item.width : 0;
+    } else if (this.item) {
+      // Loader size exists
+      this.item.width = this.width;
+    }
+
+    if (!this.height) {
+      this.height = this.item ? this.item.height : 0;
+    } else if (this.item) {
+      // Loader size exists
+      this.item.height = this.height;
+    }
+  }
 });
