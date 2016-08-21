8d142e70ca66db6a3f5233c4f4a80c30440c644c
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-08-21 13:23:49 +0300

    tests: add Signal tests for QueuedConnection
    PR-URL: https://github.com/qmlweb/qmlweb/pull/330

--- tests/QtBase/Signal.js
+++ tests/QtBase/Signal.js
@@ -53,4 +53,28 @@ describe("QtBase.Signal", function() {
     signal.execute();
     expect(result).toBe(24);
   });
+
+  it("QueuedConnection", function(done) {
+    var signal = new QmlWeb.Signal();
+    var result = 0;
+    var test1 = function() {
+      result += 1;
+    };
+    var test2 = function() {
+      result += 10;
+    };
+    signal.connect(test1, QmlWeb.Signal.QueuedConnection);
+    expect(result).toBe(0);
+    signal.execute();
+    expect(result).toBe(0);
+    signal.connect(test2, QmlWeb.Signal.QueuedConnection);
+    signal.execute();
+    expect(result).toBe(0);
+    signal.execute();
+    expect(result).toBe(0);
+    setTimeout(function() {
+      expect(result).toBe(23);
+      done();
+    }, 10);
+  });
 });
