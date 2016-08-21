21d6f8e3730dcd4258c9a35042d3d00885759e10
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-08-21 11:10:18 +0300

    Rectangle: initial cleanup
    This doesn't change the actual code logic, which looks broken though.

--- src/modules/QtQuick/Rectangle.js
+++ src/modules/QtQuick/Rectangle.js
@@ -11,101 +11,109 @@ registerQmlType({
   constructor(meta) {
     callSuper(this, meta);
 
-    this.border = new QObject(this);
-    createProperty("color", this.border, "color", {initialValue: 'black'});
-    createProperty("int", this.border, "width", {initialValue: 1});
+    this.border = new QmlWeb.QObject(this);
+    createProperty("color", this.border, "color", { initialValue: "black" });
+    createProperty("int", this.border, "width", { initialValue: 1 });
 
-    const bg = this.impl = document.createElement('div');
-    bg.style.pointerEvents = 'none';
-    bg.style.position = 'absolute';
-    bg.style.left = bg.style.right = bg.style.top = bg.style.bottom = '0px';
-    bg.style.borderWidth ='0px';
-    bg.style.borderStyle = 'solid';
-    bg.style.borderColor = 'black';
-    bg.style.backgroundColor = 'white';
+    const bg = this.impl = document.createElement("div");
+    bg.style.pointerEvents = "none";
+    bg.style.position = "absolute";
+    bg.style.left = bg.style.right = bg.style.top = bg.style.bottom = "0px";
+    bg.style.borderWidth = "0px";
+    bg.style.borderStyle = "solid";
+    bg.style.borderColor = "black";
+    bg.style.backgroundColor = "white";
     this.dom.appendChild(bg);
 
-    this.colorChanged.connect(this, function(newVal) {
-        bg.style.backgroundColor = new QmlWeb.QColor(newVal);
-    });
-    this.radiusChanged.connect(this, function(newVal) {
-        bg.style.borderRadius = newVal + 'px';
-    });
-    this.border.colorChanged.connect(this, function(newVal) {
-        bg.style.borderColor = new QmlWeb.QColor(newVal);
-        if (bg.style.borderWidth == '0px') {
-            bg.style.borderWidth = this.border.width + 'px';
-        }
-        this.$updateBorder(this.border.width);
-    });
-    this.border.widthChanged.connect(this, function(newVal) {
-        // ignore negative border width
-        if (newVal >= 0) {
-            this.$updateBorder(newVal);
-        } else {
-            bg.style.borderWidth = "0px";
-        }
-    });
-    this.widthChanged.connect(this, function(newVal){
-       this.$updateBorder(this.border.width);
-    });
-    this.heightChanged.connect(this, function(newVal){
-       this.$updateBorder(this.border.width);
-    });
+    this.colorChanged.connect(this, this.$onColorChanged);
+    this.radiusChanged.connect(this, this.$onRadiusChanged);
+    this.border.colorChanged.connect(this, this.border$onColorChanged);
+    this.border.widthChanged.connect(this, this.border$onWidthChanged);
+    this.widthChanged.connect(this, this.$updateBorder);
+    this.heightChanged.connect(this, this.$updateBorder);
   }
-  $updateBorder(newBorderWidth) {
-    const bg = this.dom.firstChild;
-
-    // ignore negative and 0px border width
-    if (newBorderWidth == "0px" || newBorderWidth < 0) {
-        return;
+  $onColorChanged(newVal) {
+    this.impl.style.backgroundColor = new QmlWeb.QColor(newVal);
+  }
+  border$onColorChanged(newVal) {
+    const style = this.impl.style;
+    style.borderColor = new QmlWeb.QColor(newVal);
+    if (style.borderWidth === "0px") {
+      style.borderWidth = `${this.border.width}px`;
     }
+    this.$updateBorder();
+  }
+  border$onWidthChanged(newVal) {
+    // ignore negative border width
+    if (newVal < 0) {
+      this.impl.style.borderWidth = "0px";
+      return;
+    }
+    this.$updateBorder();
+  }
+  $onRadiusChanged(newVal) {
+    this.impl.style.borderRadius = `${newVal}px`;
+  }
+  $updateBorder() {
+    const size = this.border.width;
+    const style = this.impl.style;
+
+    // ignore negative border width
+    if (size < 0) {
+      return;
+    }
+
     // no Rectangle border width was set yet
-    if ( (newBorderWidth == "1" && bg.style.borderWidth == "0px") || ( typeof newBorderWidth === "undefined" && bg.style.borderWidth == "0px") ) {
-        return;
+    if ((size === 1 || typeof size === "undefined") &&
+        style.borderWidth === "0px") {
+      return;
     }
 
-    var topBottom = typeof newBorderWidth === "undefined" ? bg.style.borderWidth : newBorderWidth + 'px';
-    var leftRight = topBottom;
+    let topBottom = typeof size === "undefined" ?
+                      style.borderWidth :
+                      `${size}px`;
+    let leftRight = topBottom;
 
-    bg.style.borderTopWidth = topBottom;
-    bg.style.borderBottomWidth = topBottom;
-    bg.style.borderLeftWidth = leftRight;
-    bg.style.borderRightWidth = leftRight;
+    style.borderTopWidth = topBottom;
+    style.borderBottomWidth = topBottom;
+    style.borderLeftWidth = leftRight;
+    style.borderRightWidth = leftRight;
 
     // hide border if any of dimensions is less then one
-    if (this.width <= 0 || this.height <= 0 || typeof this.width === "undefined" || typeof this.height === "undefined") {
-        bg.style.borderWidth = '0px';
+    if (this.width <= 0 || typeof this.width === "undefined" ||
+        this.height <= 0 || typeof this.height === "undefined") {
+      style.borderWidth = "0px";
     } else {
-        // check if border is not greater than Rectangle size
-        // react by change of width or height of div (in css)
-
-        if (2 * this.border.width > this.height) {
-            topBottom = this.height/2 + 'px';
-            bg.style.height = '0px';
-        } else {
-            if ( this.height - 2 * this.border.width < this.border.width) {
-                if (this.height > 2) {
-                    bg.style.height = (this.height%2 ? -1 : -2 + this.height + (this.height - (2*this.border.width))) + 'px';
-                }
-            }
+      // check if border is not greater than Rectangle size
+      // react by change of width or height of div (in css)
+      if (this.height < 2 * this.border.width) {
+        topBottom = `${this.height / 2}px`;
+        style.height = "0px";
+      } else if (this.height < 3 * this.border.width) { // TODO: what??
+        if (this.height > 2) {
+          const height = this.height % 2
+                            ? -1
+                            : -2 + 2 * this.height - 2 * this.border.width;
+          style.height = `${height}px`;
         }
+      }
 
-        if (2 * this.border.width > this.width) {
-            leftRight = this.width/2 + 'px';
-            bg.style.width = '0px';
-        } else {
-            if (this.width - 2 * this.border.width < this.border.width) {
-                if (this.width > 2) {
-                    bg.style.width = (this.width%2 ? -1 : -2 + this.width + ( this.width - (2*this.border.width))) + 'px';
-                }
-            }
+      if (this.width < 2 * this.border.width) {
+        leftRight = `${this.width / 2}px`;
+        style.width = "0px";
+      } else if (this.width < 3 * this.border.width) { // TODO: what??
+        if (this.width > 2) {
+          const width = this.width % 2
+                          ? -1
+                          : -2 + 2 * this.width - 2 * this.border.width;
+          style.width = `${width}px`;
         }
+      }
 
-        bg.style.borderTopWidth = topBottom;
-        bg.style.borderBottomWidth = topBottom;
-        bg.style.borderLeftWidth = leftRight;
-        bg.style.borderRightWidth = leftRight;
+      style.borderTopWidth = topBottom;
+      style.borderBottomWidth = topBottom;
+      style.borderLeftWidth = leftRight;
+      style.borderRightWidth = leftRight;
     }
   }
 });
