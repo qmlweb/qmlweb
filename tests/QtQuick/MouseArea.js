c56e7560144868b3fcf43a82c3bcd6459efcbd2f
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-08-23 17:43:15 +0300

    tests: Add MouseArea.CotainsMouse test

--- tests/QtQuick/MouseArea.js
+++ tests/QtQuick/MouseArea.js
@@ -2,8 +2,23 @@ describe("QtQuick.MouseArea", function() {
   setupDivElement();
   var load = prefixedQmlLoader("QtQuick/qml/MouseArea");
 
+  it("CotainsMouse", function() {
+    var qml = load("Generic", this.div);
+    expect(qml.area.containsMouse).toBe(false);
+    sendEvent(qml.area.dom, "mouseover", 0, 0);
+    expect(qml.area.containsMouse).toBe(true);
+    sendEvent(qml.area.dom, "mouseover", 10, 0);
+    expect(qml.area.containsMouse).toBe(true);
+    sendEvent(qml.area.dom, "mouseout", 0, 10);
+    expect(qml.area.containsMouse).toBe(false);
+    sendEvent(qml.area.dom, "mouseover", 10, 10);
+    expect(qml.area.containsMouse).toBe(true);
+    sendEvent(qml.area.dom, "mouseout", 0, 0);
+    expect(qml.area.containsMouse).toBe(false);
+  });
+
   it("ClickOutside", function() {
-    var qml = load("ClickOutside", this.div);
+    var qml = load("Generic", this.div);
     expect(qml.pressed).toBe(false);
     sendEvent(qml.area.dom, "mousedown", 0, 0);
     expect(qml.pressed).toBe(true);
