qmlweb/qmlweb

commit ed4f2b74b8bf098faa2cfb78237c70de4159745d

modules: cleanup some qml types, merge geometry into Item
PR-URL: https://github.com/qmlweb/qmlweb/pull/306

src/modules/QtQml/QtObject.js
@@ -1,23 +1,25 @@
 // Base object for all qml elements
 
-class QMLBaseObject extends QObject {
+registerQmlType({
+  module: "QtQml",
+  name: "QtObject",
+  versions: /.*/,
+}, class extends QObject {
   constructor(meta) {
     super(meta.parent);
-
-    var i,
-        prop;
 
     this.$isComponentRoot = meta.isComponentRoot;
     this.$context = meta.context;
 
     // Component get own properties
-    var attributes = [];
-    for (var key in meta.object) {
-      if (meta.object.hasOwnProperty(key) &&
-          typeof meta.object[key] != 'undefined' && meta.object[key] != null &&
-          (meta.object[key].__proto__.constructor.name == 'QMLPropertyDefinition' ||
-           meta.object[key].__proto__.constructor.name == 'QMLAliasDefinition')) {
-        attributes.push(key);
+    this.$attributes = [];
+    for (const key in meta.object) {
+      if (!meta.object.hasOwnProperty(key) || !meta.object[key]) {
+        continue;
+      }
+      const name = meta.object[key].__proto__.constructor.name;
+      if (name !== "QMLPropertyDefinition" && name !== "QMLAliasDefinition") {
+        this.$attributes.push(key);
       }
     }
 
@@ -28,8 +30,9 @@ class QMLBaseObject extends QObject {
     this.Keys.callPressed = Signal.signal();
     this.Keys.cancelPressed = Signal.signal();
     this.Keys.deletePressed = Signal.signal();
-    for (var i = 0 ; i < 10 ; ++i)
-      this.Keys['digit'+i+'Pressed'] = Signal.signal();
+    for (let i = 0; i < 10; ++i) {
+      this.Keys[`digit${i}Pressed`] = Signal.signal();
+    }
     this.Keys.escapePressed = Signal.signal();
     this.Keys.flipPressed = Signal.signal();
     this.Keys.hangupPressed = Signal.signal();
@@ -47,14 +50,8 @@ class QMLBaseObject extends QObject {
     this.Keys.volumeDownPressed = Signal.signal();
     this.Keys.volumeUpPressed = Signal.signal();
     this.Keys.yesPressed = Signal.signal();
-
-    this.getAttributes = function() { return (attributes); }
   }
-}
-
-registerQmlType({
-    module: 'QtQml',
-    name: 'QtObject',
-    versions: /.*/,
-    constructor: QMLBaseObject
+  getAttributes() {
+    return this.$attributes;
+  }
 });

gulpfile.js
@@ -37,6 +37,12 @@ const tests = [
 
 const js = [
   "*.js",
+  "src/modules/QmlWeb*/*.js",
+  "src/modules/Qt.labs.*/*.js",
+  "src/modules/QtGraphicalEffects/*.js",
+  "src/modules/QtMobility/*.js",
+  "src/modules/QtQuick.Window/*.js",
+  "src/modules/QtWebSockets/*.js",
   "tests/**/*.js"
 ];
 

src/engine/QMLEngine.js
@@ -52,6 +52,7 @@ class QMLEngine {
     //----------Construct----------
 
     // TODO: Move to module initialization
+    const QMLBaseObject = getConstructor("QtQml", "2.0", "QtObject");
     for (const i in constructors) {
       if (constructors[i].getAttachedObject) {
         setupGetter(QMLBaseObject.prototype, i, constructors[i].getAttachedObject);
@@ -456,15 +457,15 @@ class QMLEngine {
         property.update();
       } else if (geometryProperties.indexOf(property.name) >= 0) {
         // It is possible that bindings with these names was already evaluated
-        // during eval of other bindings but in that case updateHGeometry and
-        // updateVGeometry could be blocked during their eval.
+        // during eval of other bindings but in that case $updateHGeometry and
+        // $updateVGeometry could be blocked during their eval.
         // So we call them explicitly, just in case.
-
-        if (property.changed.isConnected(property.obj, updateHGeometry)) {
-          updateHGeometry.apply(property.obj, [property.val, property.val, property.name]);
+        const { obj, changed } = property;
+        if (obj.$updateHGeometry && changed.isConnected(obj, obj.$updateHGeometry)) {
+          obj.$updateHGeometry(property.val, property.val, property.name);
         }
-        if (property.changed.isConnected(property.obj, updateVGeometry)) {
-          updateVGeometry.apply(property.obj, [property.val, property.val, property.name]);
+        if (obj.$updateVGeometry && changed.isConnected(obj, obj.$updateVGeometry)) {
+          obj.$updateVGeometry(property.val, property.val, property.name);
         }
       }
     }

src/modules/QmlWeb.Dom/DomElement.js
@@ -9,7 +9,8 @@ registerQmlType({
 }, class {
   constructor(meta) {
     callSuper(this, meta);
-    var tagName = meta.object.tagName || 'div';
+
+    const tagName = meta.object.tagName || "div";
     this.dom = document.createElement(tagName);
 
     // TODO: support properties, styles, perhaps changing the tagName

src/modules/QmlWeb/RestModel.js
@@ -16,137 +16,123 @@ registerQmlType({
 }, class {
   constructor(meta) {
     callSuper(this, meta);
-    var self = this;
-    var attributes = this.getAttributes();
-    this.attributes    = attributes;
 
+    this.attributes = this.getAttributes();
     this.runningRequests = 0;
-
-    this.fetch = function() {
-      ajax({
-        method:   'GET',
-        mimeType: self.mimetype,
-        success: function(xhr) {
-          xhrReadResponse(xhr);
-          self.fetched();
+  }
+  fetch() {
+    this.$ajax({
+      method: "GET",
+      mimeType: this.mimetype,
+      success: xhr => {
+        this.$xhrReadResponse(xhr);
+        this.fetched();
+      }
+    });
+  }
+  remove() {
+    this.$ajax({
+      method: "DELETE",
+      success: xhr => {
+        this.destroy();
+      }
+    });
+  }
+  create() {
+    $this.sendToServer("POST");
+  }
+  save() {
+    $this.sendToServer("PUT");
+  }
+  $sendToServer(method) {
+    this.$ajax({
+      method,
+      mimeType: this.queryMimeType,
+      body: this.$generateBodyForPostQuery(),
+      success: xhr => {
+        this.$xhrReadResponse(xhr);
+        this.saved();
+      }
+    });
+  }
+  $generateBodyForPostQuery() {
+    const object = {};
+    for (let i = 0; i < this.attributes.length; ++i) {
+      object[this.attributes[i]] = this.$properties[this.attributes[i]].get();
+    }
+    console.log(object);
+    switch (this.queryMimeType) {
+      case "application/json":
+      case "text/json":
+        return JSON.stringify(object);
+      case "application/x-www-urlencoded":
+        return this.$objectToUrlEncoded(object);
+    }
+    return undefined;
+  }
+  $objectToUrlEncoded(object, prefix) {
+    const parts = [];
+    for (let key in object) {
+      if (object.hasOwnProperty(key)) {
+        const value = object[key];
+        if (typeof prefix !== "undefined") {
+          key = `${prefix}[${key}]`;
         }
-      });
+        if (typeof value === "object") {
+          parts.push(this.$objectToUrlEncoded(value, key));
+        } else {
+          parts.push(`${this.$myEncodeURIComponent(key)}=${this.$myEncodeURIComponent(value)}`);
+        }
+      }
+    }
+    return parts.join("&");
+  }
+  $myEncodeURIComponent(str) {
+    return encodeURIComponent(str).replace(/[!'()*]/g, c =>
+      `%${c.charCodeAt(0).toString(16)}`
+    );
+  }
+  $ajax(options) {
+    const xhr = new XMLHttpRequest();
+    xhr.overrideMimeType(this.mimeType);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          options.success(xhr);
+        } else {
+          options.failure(xhr);
+        }
+        this.runningRequests -= 1;
+        if (this.runningRequests <= 0) {
+          this.isLoading = false;
+        }
+      }
     };
-
-    this.create = function() {
-      sendToServer('POST');
-    };
-
-    this.save = function() {
-      sendToServer('PUT');
-    };
-
-    function sendToServer(method) {
-      var body = generateBodyForPostQuery();
-
-      ajax({
-        method:   method,
-        mimeType: self.queryMimeType,
-        body:     body,
-        success:  function(xhr) {
-          xhrReadResponse(xhr);
-          self.saved();
-        }
-      });
+    xhr.open(options.method, this.url, true);
+    if (typeof options.body !== "undefined") {
+      xhr.setRequestHeader("Content-Type", this.queryMimeType);
+      xhr.send(options.body);
+    } else {
+      xhr.send(null);
     }
-
-    this.remove = function() {
-      ajax({
-        method: 'DELETE',
-        success: function(xhr) {
-          self.destroy();
-        }
-      });
-    };
-
-    function generateBodyForPostQuery() {
-      var object     = {};
-      var body;
-
-      for (var i = 0 ; i < self.attributes.length ; ++i)
-        object[self.attributes[i]] = self.$properties[self.attributes[i]].get();
-      console.log(object);
-      if (self.queryMimeType == 'application/json' || self.queryMimeType == 'text/json')
-        body = JSON.stringify(object);
-      else if (self.queryMimeType == 'application/x-www-urlencoded')
-        body = objectToUrlEncoded(object);
-      return body;
+    this.runningRequests += 1;
+    this.isLoading = true;
+  }
+  $xhrReadResponse(xhr) {
+    let responseObject;
+    if (this.mimeType === "application/json" || this.mimeType === "text/json") {
+      responseObject = JSON.parse(xhr.responseText);
     }
-
-    function myEncodeURIComponent(str) {
-      return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
-        return '%' + c.charCodeAt(0).toString(16);
-      });
-    }
-
-    function objectToUrlEncoded(object, prefix) {
-      var str = '';
-      for (var key in object) {
-        if (object.hasOwnProperty(key)) {
-          var value = object[key];
-          if (str != '')
-            str += '&';
-          if (typeof prefix != 'undefined')
-            key  = prefix + '[' + key + ']';
-          if (typeof value == 'object')
-            str += objectToUrlEncoded(value, key);
-          else
-            str += myEncodeURIComponent(key) + '=' + myEncodeURIComponent(value);
-        }
-      }
-      return str;
-    }
-
-    function ajax(options) {
-      var xhr = new XMLHttpRequest();
-
-      xhr.overrideMimeType(self.mimeType);
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-          if (xhr.status == 200)
-            options.success(xhr);
-          else
-            options.failure(xhr);
-          self.runningRequests -= 1;
-          if (self.runningRequests <= 0)
-            self.isLoading = false;
-        }
-      }
-      xhr.open(options.method, self.url, true);
-      if (typeof options.body != 'undefined') {
-        xhr.setRequestHeader('Content-Type', self.queryMimeType);
-        xhr.send(options.body);
-      }
-      else
-        xhr.send(null);
-      self.runningRequests += 1;
-      self.isLoading = true;
-    }
-
-    function xhrReadResponse(xhr) {
-      var responseObject;
-
-      if (self.mimeType == 'application/json' || self.mimeType == 'text/json') {
-        responseObject = JSON.parse(xhr.responseText);
-      }
-      updatePropertiesFromResponseObject(responseObject);
-    }
-
-    function updatePropertiesFromResponseObject(responseObject) {
-      for (var key in responseObject) {
-        if (responseObject.hasOwnProperty(key) && self.$hasProperty(key)) {
-          self.$properties[key].set(responseObject[key], QMLProperty.ReasonUser);
-        }
+    this.$updatePropertiesFromResponseObject(responseObject);
+  }
+  $updatePropertiesFromResponseObject(responseObject) {
+    for (const key in responseObject) {
+      if (responseObject.hasOwnProperty(key) && this.$hasProperty(key)) {
+        this.$properties[key].set(responseObject[key], QMLProperty.ReasonUser);
       }
     }
-
-    this.$hasProperty = function(name) {
-      return (typeof self.$properties[name] != 'undefined');
-    }
+  }
+  $hasProperty(name) {
+    return typeof this.$properties[name] !== "undefined";
   }
 });

src/modules/Qt.labs.settings/Settings.js
@@ -10,41 +10,38 @@ registerQmlType({
   constructor(meta) {
     callSuper(this, meta);
 
-    if (typeof window.localStorage == 'undefined')
-      return ;
+    if (typeof window.localStorage === "undefined") {
+      return;
+    }
 
-    var attributes;
+    this.Component.completed.connect(this, this.Component$onCompleted);
+  }
+  Component$onCompleted() {
+    this.$loadProperties();
+    this.$initializeProperties();
+  }
+  $getKey(attrName) {
+    return `${this.category}/${attrName}`;
+  }
+  $loadProperties() {
+    for (let i = 0; i < this.$attributes.length; ++i) {
+      const key = this.$getKey(this.$attributes[i]);
+      this[this.$attributes[i]] = localStorage.getItem(key);
+    }
+  }
+  $initializeProperties() {
+    this.$attributes.forEach(attrName => {
+      let emitter = this;
+      let signalName = `${attrName}Changed`;
 
-    var getKey = (function(attrName) {
-      return this.category + '/' + attrName;
-    }).bind(this);
-
-    var loadProperties = (function() {
-      for (var i = 0 ; i < attributes.length ; ++i) {
-        this[attributes[i]] = localStorage.getItem(getKey(attributes[i]));
+      if (this.$properties[attrName].type === "alias") {
+        emitter = this.$context[this.$properties[attrName].val.objectName];
+        signalName = `${this.$properties[attrName].val.propertyName}Changed`;
       }
-    }).bind(this);
 
-    var initializeProperties = (function() {
-      for (var i = 0 ; i < attributes.length ; ++i) {
-        var attrName   = attributes[i];
-        var signalName = attrName + 'Changed';
-        var emitter    = this;
-
-        if (this.$properties[attrName].type == 'alias') {
-          emitter    = this.$context[this.$properties[attrName].val.objectName];
-          signalName = this.$properties[attrName].val.propertyName + 'Changed';
-        }
-        emitter[signalName].connect(this, (function() {
-          localStorage.setItem(getKey(this.attrName), this.self[this.attrName]);
-        }).bind({ self: this, attrName: attrName }));
-      }
-    }).bind(this);
-
-    this.Component.completed.connect(this, (function() {
-      attributes = this.getAttributes();
-      loadProperties();
-      initializeProperties();
-    }).bind(this));
+      emitter[signalName].connect(this, () => {
+        localStorage.setItem(this.$getKey(attrName), this[attrName]);
+      });
+    });
   }
 });

src/modules/QtGraphicalEffects/FastBlur.js
@@ -11,46 +11,43 @@ registerQmlType({
   constructor(meta) {
     callSuper(this, meta);
 
-    var previousSource = null;
-    var filterObject;
+    this.$previousSource = null;
+    this.$filterObject = undefined;
 
-    var updateFilterObject = (function() {
-      filterObject = {
-        transformType: 'filter',
-        operation:     'blur',
-        parameters:    this.radius + 'px'
-      };
-    }).bind(this);
-
-    function stripEffectFromSource(source) {
-      if (previousSource != null) {
-        var index = previousSource.transform.indexOf(filterObject);
-
-        previousSource.transform.splice(index, 1);
-        previousSource.$updateTransform();
-      }
+    this.radiusChanged.connect(this, this.$onRadiusChanged);
+    this.sourceChanged.connect(this, this.$onSourceChanged);
+  }
+  $onRadiusChanged(newVal) {
+    this.$updateEffect(this.source);
+  }
+  $onSourceChanged(newVal) {
+    this.$updateEffect(this.source);
+  }
+  $updateFilterObject() {
+    this.$filterObject = {
+      transformType: "filter",
+      operation: "blur",
+      parameters: `${this.radius}px`
+    };
+  }
+  $stripEffectFromSource(source) {
+    if (this.$previousSource) {
+      const index = this.$previousSource.transform.indexOf(this.$filterObject);
+      this.$previousSource.transform.splice(index, 1);
+      this.$previousSource.$updateTransform();
     }
-
-    function updateEffect(source) {
-      console.log("updating effect");
-      stripEffectFromSource(previousSource);
-      if (source != null && typeof source.transform != 'undefined') {
-        updateFilterObject();
-        console.log("updating effect:", filterObject, source);
-        source.transform.push(filterObject);
-        source.$updateTransform();
-        previousSource = source;
-      } else {
-        previousSource = null;
-      }
+  }
+  $updateEffect(source) {
+    console.log("updating effect");
+    this.stripEffectFromSource(this.$previousSource);
+    if (source && source.transform) {
+      this.$updateFilterObject();
+      console.log("updating effect:", this.$filterObject, source);
+      source.transform.push(this.$filterObject);
+      source.$updateTransform();
+      this.$previousSource = source;
+    } else {
+      this.$previousSource = null;
     }
-
-    this.radiusChanged.connect(this, (function(newVal) {
-      updateEffect(this.source);
-    }).bind(this));
-
-    this.sourceChanged.connect(this, (function(newVal) {
-      updateEffect(this.source);
-    }).bind(this));
   }
 });

src/modules/QtMobility/GeoLocation.js
@@ -17,24 +17,22 @@ registerQmlType({
 }, class {
   constructor(meta) {
     callSuper(this, meta);
-    var self = this;
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition
+    if (!navigator.geolocation) {
+      return;
     }
 
-    var updatePosition = (function(position) {
-      this.accuracy         = position.coords.accuracy;
-      this.altitude         = position.coords.altitude;
-      this.altitudeAccuracy = position.coords.altitudeAccuracy;
-      this.heading          = position.coords.heading;
-      this.latitude         = position.coords.latitude;
-      this.longitude        = position.coords.longitude;
-      this.speed            = position.coords.speed;
-      this.timestamp        = position.timestamp;
-    }).bind(this);
-
-    navigator.geolocation.getCurrentPosition(updatePosition);
-    navigator.geolocation.watchPosition(updatePosition);
+    navigator.geolocation.getCurrentPosition(pos => this.$updatePosition(pos));
+    navigator.geolocation.watchPosition(pos => this.$updatePosition(pos));
+  }
+  $updatePosition(position) {
+    this.accuracy = position.coords.accuracy;
+    this.altitude = position.coords.altitude;
+    this.altitudeAccuracy = position.coords.altitudeAccuracy;
+    this.heading = position.coords.heading;
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+    this.speed = position.coords.speed;
+    this.timestamp = position.timestamp;
   }
 });

commit d32b693b15b09b14c6dcad7bef845a5e500ca932

QtObject: fix $attributes
Those were broken since ed4f2b74b8bf098faa2cfb78237c70de4159745d

src/modules/QtQml/QtObject.js
@@ -18,7 +18,7 @@ QmlWeb.registerQmlType({
         continue;
       }
       const name = meta.object[key].__proto__.constructor.name;
-      if (name !== "QMLPropertyDefinition" && name !== "QMLAliasDefinition") {
+      if (name === "QMLPropertyDefinition" || name === "QMLAliasDefinition") {
         this.$attributes.push(key);
       }
     }