ffdf42ed4c50efecd6c329f25a78fa0067b4a655
Stephen D'Angelo <stephen.m.dangelo@gmail.com>
2016-10-12 10:17:04 -0400

    QtQml/Connections: Only reconnect signals if target is valid
    Per the documentation, if target is set to null, the connection should
    be ignored.
    
    Refs: http://doc.qt.io/qt-5/qml-qtqml-connections.html#target-prop
    PR-URL: https://github.com/qmlweb/qmlweb/pull/344
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- tests/QtQml/Connections.js
+++ tests/QtQml/Connections.js
@@ -2,13 +2,24 @@ describe("QtQml.Connections", function() {
   setupDivElement();
   var load = prefixedQmlLoader("QtQml/qml/Connections");
 
-  it("Connections", function() {
+  it("Connections default target is parent", function() {
     var qml = load("Connections", this.div);
     expect(qml.connections.target).toBe(qml);
     qml.value = 1;
     expect(qml.test_value).toBe(1);
+  });
+
+  it("Connections target change", function() {
+    var qml = load("Connections", this.div);
     qml.connections.target = qml.new_target;
     qml.new_target.value = 1;
-    expect(qml.test_value).toBe(2);
+    expect(qml.test_value).toBe(1);
+  });
+
+  it("Connections null target", function() {
+    var qml = load("Connections", this.div);
+    qml.connections.target = null;
+    qml.value = 1;
+    expect(qml.test_value).toBe(0);
   });
 });
