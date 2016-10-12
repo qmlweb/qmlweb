aa059ee2f900607aab44379c2411d8c70c0e624b
Stephen D'Angelo <stephen.m.dangelo@gmail.com>
2016-10-12 14:25:27 -0400

    modules/Text: Properly initialize wrapMode
    Call $onWrapModeChanged to initialize CSS properties for the default
    wrapMode of NoWrap.
    
    Also removed double connections to text, wrapMode changed.

--- tests/QtQuick/Text.js
+++ tests/QtQuick/Text.js
@@ -6,5 +6,10 @@ describe("QtQuick.Text", function() {
     var qml = load("ImplicitSize", this.div);
     expect(qml.text_item.width).toBeGreaterThan(0);
   });
+
+  it("default wrap mode", function() {
+    var qml = load("WrapMode", this.div);
+    expect(qml.dom.children[0].style.whiteSpace).toBe("pre");
+  });
 });
 
