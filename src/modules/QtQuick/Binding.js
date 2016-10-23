e7adc9a2722a6cc0c02ba7a9d5a501b21bc81c9c
Alexander Rössler <mail.aroessler@gmail.com>
2016-10-23 18:21:09 +0200

    implement QtQuick Binding

---
+++ src/modules/QtQuick/Binding.js
@@ -0,0 +1,38 @@
+QmlWeb.registerQmlType({
+  module: "QtQuick",
+  name: "Binding",
+  versions: /.*/,
+  baseClass: "QtQml.QtObject",
+  properties: {
+    target: { type: "QtObject", initialValue: null },
+    property: { type: "string", initialValue: "" },
+    value: { type: "var", initialValue: null },
+    when: { type: "bool", initialValue: true }
+  }
+}, class {
+  constructor(meta) {
+    QmlWeb.callSuper(this, meta);
+
+    this.$property = undefined;
+
+    this.valueChanged.connect(this, this.$onValueChanged);
+    this.targetChanged.connect(this, this.$updateBinding);
+    this.propertyChanged.connect(this, this.$updateBinding);
+    this.whenChanged.connect(this, this.$updateBinding);
+  }
+
+  $updateBinding() {
+    if (!this.when || this.target === null) {
+      this.$property = undefined;
+      return;
+    }
+    this.$property = this.target.$properties[this.property];
+  }
+
+  $onValueChanged(value) {
+    if (this.$property !== undefined && this.$property.val !== value) {
+      this.$property.val = value;
+      this.$property.changed(value);
+    }
+  }
+});
