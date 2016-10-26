b8c1022dbdc1b0beedac1f9c4a4bafa4d3f2d7e7
Stephen D'Angelo <stephen.m.dangelo@gmail.com>
2016-10-26 14:25:03 -0400

    QMLEngine.$resolvePath: If file is falsey, just return it
    This better mimics Qt QML where an unset url property (or one set to an
    empty string) == "".

--- tests/QMLEngine/properties.js
+++ tests/QMLEngine/properties.js
@@ -110,5 +110,7 @@ describe("QMLEngine.properties", function() {
     const a = document.createElement("a");
     a.href = "/";
     expect(qml.absolute).toBe(a.href + "absolute-url");
+    expect(qml.unset).toBe("");
+    expect(qml.setToEmptyString).toBe("");
   });
 });
