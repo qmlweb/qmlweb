9df6732358737ecb6a2312a7aa19d00d984d6bf1
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-12-14 02:58:59 +0300

    QtQuick.Window: initial Window stub impl

---
+++ src/modules/QtQuick.Window/Window.js
@@ -0,0 +1,33 @@
+QmlWeb.registerQmlType({
+  module: "QtQuick.Window",
+  name: "Window",
+  versions: /^2\./,
+  baseClass: "QtQuick.Item",
+  properties: {
+    active: "bool",
+    activeFocusItem: "Item",
+    color: { type: "color", initialValue: "#ffffff" },
+    //contentItem: "Item", // TODO
+    contentOrientation: "enum",
+    flags: "int",
+    maximumHeight: "int",
+    maximumWidth: "int",
+    minimumHeight: "int",
+    minimumWidth: "int",
+    modality: "enum",
+    title: "string",
+    visibility: "enum"
+  },
+  signals: {
+    closing: [{ type: "CloseEvent", name: "close" }]
+  }
+}, class {
+  constructor(meta) {
+    QmlWeb.callSuper(this, meta);
+
+    this.colorChanged.connect(this, this.$onColorChanged);
+  }
+  $onColorChanged(newVal) {
+    this.dom.style.backgroundColor = newVal;
+  }
+});
