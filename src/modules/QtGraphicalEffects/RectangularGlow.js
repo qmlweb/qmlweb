216391c914be88226cf6e0e6ed4a5e6924caa369
Gaubee <GaubeeBangeel@Gmail.com>
2017-01-06 20:13:40 +0800

    Create RectangularGlow.js
    use css `box-shadow` and `filter:blur` to simulate **Glow** effect.
    They are very similar, but not exactly the same.
    
    ## REFERENCES:
    [qml-qtgraphicaleffects-rectangularglow](http://doc.qt.io/qt-5/qml-qtgraphicaleffects-rectangularglow.html)
    [css-box-shadow](https://developer.mozilla.org/zh-CN/docs/Web/CSS/box-shadow)

---
+++ src/modules/QtGraphicalEffects/RectangularGlow.js
@@ -0,0 +1,92 @@
+QmlWeb.registerQmlType({
+  module: "QtQuick",
+  name: "RectangularGlow",
+  versions: /.*/,
+  baseClass: "Item",
+  properties: {
+    cached: {
+      type: "bool",
+      initialValue: false
+    },
+    color: {
+      type: "color",
+      initialValue: "white"
+    },
+    cornerRadius: {
+      type: "real",
+      initialValue: 0
+    },
+    // This property defines how many pixels outside the item area are reached by the glow.
+    glowRadius: {
+      type: "real",
+      initialValue: 0
+    },
+    spread: {
+      type: "real",
+      initialValue: 0
+    },
+  }
+}, class {
+  constructor(meta) {
+    QmlWeb.callSuper(this, meta);
+
+    const bg = this.impl = document.createElement("div");
+    bg.style.pointerEvents = "none";
+    bg.style.position = "absolute";
+    bg.style.left = bg.style.right = bg.style.top = bg.style.bottom = "0px";
+    bg.style.border = "none";
+    bg.style.backgroundColor = "black";
+    this.dom.appendChild(bg);
+
+    this.colorChanged.connect(this, this.$onColorChanged);
+    this.glowRadiusChanged.connect(this, this.$updateBoxShadow);
+    this.cornerRadiusChanged.connect(this, this.$updateBoxShadow);
+    this.widthChanged.connect(this, this.$updateBoxShadow);
+    this.heightChanged.connect(this, this.$updateBoxShadow);
+    this.spreadChanged.connect(this, this.$onSpreadChanged);
+  }
+  $onColorChanged(newVal) {
+    this.impl.style.backgroundColor = new QmlWeb.QColor(newVal);
+    this.$updateBoxShadow();
+  }
+  $onSpreadChanged(newVal) {
+    if (newVal > 1) {
+      this.spread = 1
+    } else if (newVal < 0) {
+      this.spread = 0
+    }
+    this.$updateBoxShadow();
+  }
+
+  $updateBoxShadow() {
+
+    function calcBoxShadow(color, glowR, cornerR, spread) {
+      var totle = (glowR + cornerR * (1 - spread));
+      var glow = (1 - spread) * totle;
+      var blur_radius = glow * 0.64;
+      var spread_radius = totle - blur_radius;
+      var glow2 = glowR / 5;
+      var blur_radius_2 = glow2 * 0.8;
+      var spread_radius_2 = glow2 - blur_radius_2;
+      return `${color} 0px 0px ${blur_radius}px ${spread_radius}px,
+            ${color} 0px 0px ${blur_radius_2}px ${spread_radius_2}px`
+    }
+
+    function calcGlowCss(color, glowR, cornerR, spread, width, height) {
+      var spread_cornerR = cornerR * (1 - spread);
+      var rest_cornerR = cornerR - spread_cornerR;
+      return {
+        boxShadow: calcBoxShadow(color, glowR, cornerR, spread),
+        width: `${width - spread_cornerR}px`,
+        height: `${height - spread_cornerR}px`,
+        top: `${spread_cornerR / 2}px`,
+        left: `${spread_cornerR / 2}px`,
+        filter: `blur(${spread_cornerR / 2}px)`,
+        borderRadius: `${cornerR / 2}px`,
+        transform: `scale(${(width - spread_cornerR / 4) / width},${(height - spread_cornerR / 4) / height})`,
+      }
+    }
+
+    Object.assign(this.impl.style, calcGlowCss(this.color, this.glowRadius, this.cornerRadius, this.spread, this.width, this.height));
+  }
+});
