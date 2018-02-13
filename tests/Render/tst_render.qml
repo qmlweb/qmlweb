qt/qtdeclarative

commit 215c0145be7119a0e885ff95a17ee7aebd17ad12

qmltest: Enumerate test cases / functions without evaluating QML
Most, if not all, QML tests are written without any sort of dynamic
instantiation of the test data, so doing view.setSource() will evaluate
the whole source file, compute bindings, create items, windows, etc.

This is less then ideal when all you want is to list the test functions
using -functions, or when running a single test from the command line,
as in both cases we'll still actually evaluate every single QML file.

This makes it really hard to evaluate test output, e.g. from the CI,
especially with logging enabled, as even if a single test is requested,
the logs are filled with results from the loading of the other tests.

To improve the situation we use a non-instantiated QML component that
we then inspect its compilation data, looking for test cases and
functions.

In the future the implementation of TestCase's qtest_run* machinery
should be built on top of QTestLib instead of being reimplemented in
JavaScript, but this is left for later.

Change-Id: Ie5448208daf786e335583ab6bdfbc195891ec1f5
Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

src/imports/testlib/TestCase.qml
@@ -1739,11 +1739,6 @@ Item {
 
     /*! \internal */
     function qtest_run() {
-        if (util.printAvailableFunctions) {
-            completed = true
-            return
-        }
-
         if (TestLogger.log_start_test()) {
             qtest_results.reset()
             qtest_results.testCaseName = name
@@ -1894,29 +1889,9 @@ Item {
         }
     }
 
-
     Component.onCompleted: {
         QTestRootObject.hasTestCase = true;
         qtest_componentCompleted = true;
-
-        if (util.printAvailableFunctions) {
-            var testList = []
-            for (var prop in testCase) {
-                if (prop.indexOf("test_") != 0 && prop.indexOf("benchmark_") != 0)
-                    continue
-                var tail = prop.lastIndexOf("_data");
-                if (tail != -1 && tail == (prop.length - 5))
-                    continue
-                // Note: cannot run functions in TestCase elements
-                // that lack a name.
-                if (name.length > 0)
-                    testList.push(name + "::" + prop + "()")
-            }
-            testList.sort()
-            for (var index in testList)
-                console.log(testList[index])
-            return
-        }
         qtest_testId = TestLogger.log_register_test(name)
         if (optional)
             TestLogger.log_optional_test(qtest_testId)

src/qml/qml/qqmltypenamecache_p.h
@@ -81,7 +81,7 @@ struct QQmlImportRef {
 
 class QQmlType;
 class QQmlEngine;
-class QQmlTypeNameCache : public QQmlRefCount
+class Q_QML_PRIVATE_EXPORT QQmlTypeNameCache : public QQmlRefCount
 {
 public:
     QQmlTypeNameCache(const QQmlImports &imports);

src/qmltest/quicktest.cpp
@@ -63,6 +63,8 @@
 #include <QtCore/QTranslator>
 #include <QtTest/QSignalSpy>
 
+#include <private/qqmlcomponent_p.h>
+
 #ifdef QT_QMLTEST_WITH_WIDGETS
 #include <QtWidgets/QApplication>
 #endif
@@ -194,6 +196,133 @@ bool qWaitForSignal(QObject *obj, const char* signal, int timeout = 5000)
 
     return spy.size();
 }
+
+using namespace QV4::CompiledData;
+
+class TestCaseCollector
+{
+public:
+    typedef QList<QString> TestCaseList;
+
+    TestCaseCollector(const QFileInfo &fileInfo, QQmlEngine *engine)
+    {
+        QQmlComponent component(engine, fileInfo.absoluteFilePath());
+        m_errors += component.errors();
+
+        if (component.isReady()) {
+            CompilationUnit *rootCompilationUnit = QQmlComponentPrivate::get(&component)->compilationUnit;
+            TestCaseEnumerationResult result = enumerateTestCases(rootCompilationUnit);
+            m_testCases = result.testCases + result.finalizedPartialTestCases();
+            m_errors += result.errors;
+        }
+    }
+
+    TestCaseList testCases() const { return m_testCases; }
+    QList<QQmlError> errors() const { return m_errors; }
+
+private:
+    TestCaseList m_testCases;
+    QList<QQmlError> m_errors;
+
+    struct TestCaseEnumerationResult
+    {
+        TestCaseList testCases;
+        QList<QQmlError> errors;
+
+        // Partially constructed test cases
+        bool isTestCase = false;
+        TestCaseList testFunctions;
+        QString testCaseName;
+
+        TestCaseList finalizedPartialTestCases() const
+        {
+            TestCaseList result;
+            for (const QString &function : testFunctions)
+                result << QString(QStringLiteral("%1::%2")).arg(testCaseName).arg(function);
+            return result;
+        }
+
+        TestCaseEnumerationResult &operator<<(const TestCaseEnumerationResult &other)
+        {
+            testCases += other.testCases + other.finalizedPartialTestCases();
+            errors += other.errors;
+            return *this;
+        }
+    };
+
+    TestCaseEnumerationResult enumerateTestCases(CompilationUnit *compilationUnit, const Object *object = nullptr)
+    {
+        QQmlType testCaseType;
+        for (quint32 i = 0; i < compilationUnit->data->nImports; ++i) {
+            const Import *import = compilationUnit->data->importAt(i);
+            if (compilationUnit->stringAt(import->uriIndex) != QLatin1Literal("QtTest"))
+                continue;
+
+            QString testCaseTypeName(QStringLiteral("TestCase"));
+            QString typeQualifier = compilationUnit->stringAt(import->qualifierIndex);
+            if (!typeQualifier.isEmpty())
+                testCaseTypeName = typeQualifier % QLatin1Char('.') % testCaseTypeName;
+
+            testCaseType = compilationUnit->typeNameCache->query(testCaseTypeName).type;
+            if (testCaseType.isValid())
+                break;
+        }
+
+        TestCaseEnumerationResult result;
+
+        if (!object) // Start at root of compilation unit if not enumerating a specific child
+            object = compilationUnit->objectAt(compilationUnit->rootObjectIndex());
+
+        if (CompilationUnit *superTypeUnit = compilationUnit->resolvedTypes.value(object->inheritedTypeNameIndex)->compilationUnit) {
+            // We have a non-C++ super type, which could indicate we're a subtype of a TestCase
+            if (testCaseType.isValid() && superTypeUnit->url() == testCaseType.sourceUrl())
+                result.isTestCase = true;
+            else
+                result = enumerateTestCases(superTypeUnit);
+
+            if (result.isTestCase) {
+                // Look for override of name in this type
+                for (auto binding = object->bindingsBegin(); binding != object->bindingsEnd(); ++binding) {
+                    if (compilationUnit->stringAt(binding->propertyNameIndex) == QLatin1Literal("name")) {
+                        if (binding->type == QV4::CompiledData::Binding::Type_String) {
+                            result.testCaseName = compilationUnit->stringAt(binding->stringIndex);
+                        } else {
+                            QQmlError error;
+                            error.setUrl(compilationUnit->url());
+                            error.setLine(binding->location.line);
+                            error.setColumn(binding->location.column);
+                            error.setDescription(QStringLiteral("the 'name' property of a TestCase must be a literal string"));
+                            result.errors << error;
+                        }
+                        break;
+                    }
+                }
+
+                // Look for additional functions in this type
+                auto functionsEnd = compilationUnit->objectFunctionsEnd(object);
+                for (auto function = compilationUnit->objectFunctionsBegin(object); function != functionsEnd; ++function) {
+                    QString functionName = compilationUnit->stringAt(function->nameIndex);
+                    if (!(functionName.startsWith(QLatin1Literal("test_")) || functionName.startsWith(QLatin1Literal("benchmark_"))))
+                        continue;
+
+                    if (functionName.endsWith(QLatin1Literal("_data")))
+                        continue;
+
+                    result.testFunctions << functionName;
+                }
+            }
+        }
+
+        for (auto binding = object->bindingsBegin(); binding != object->bindingsEnd(); ++binding) {
+            if (binding->type == QV4::CompiledData::Binding::Type_Object) {
+                const Object *child = compilationUnit->objectAt(binding->value.objectIndex);
+                result << enumerateTestCases(compilationUnit, child);
+            }
+        }
+
+        return result;
+    }
+};
 
 int quick_test_main(int argc, char **argv, const char *name, const char *sourceDir)
 {
@@ -339,7 +468,28 @@ int quick_test_main(int argc, char **argv, const char *name, const char *sourceD
         if (!fi.exists())
             continue;
 
-        QQuickView view ;
+        QQmlEngine engine;
+
+        TestCaseCollector testCaseCollector(fi, &engine);
+        if (!testCaseCollector.errors().isEmpty()) {
+            for (const QQmlError &error : testCaseCollector.errors())
+                qWarning() << error;
+            exit(1);
+        }
+
+        TestCaseCollector::TestCaseList availableTestFunctions = testCaseCollector.testCases();
+        if (QTest::printAvailableFunctions) {
+            for (const QString &function : availableTestFunctions)
+                qDebug("%s()", qPrintable(function));
+            continue;
+        }
+
+        static const QSet<QString> commandLineTestFunctions = QTest::testFunctions.toSet();
+        if (!commandLineTestFunctions.isEmpty() &&
+            !availableTestFunctions.toSet().intersects(commandLineTestFunctions))
+            continue;
+
+        QQuickView view(&engine, nullptr);
         view.setFlags(Qt::Window | Qt::WindowSystemMenuHint
                          | Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint
                          | Qt::WindowCloseButtonHint);
@@ -364,8 +514,6 @@ int quick_test_main(int argc, char **argv, const char *name, const char *sourceD
         else
             view.setSource(QUrl::fromLocalFile(path));
 
-        if (QTest::printAvailableFunctions)
-            continue;
         while (view.status() == QQuickView::Loading)
             QTest::qWait(10);
         if (view.status() == QQuickView::Error) {

qmlweb/qmlweb

commit a13fb93727877a1e8e3da7966f4a0c13473c3eaa

tests: rewrite Qt-based render tests runner to support 5.10
Broken by:
https://github.com/qt/qtdeclarative/commit/215c0145be7119a0e885ff95a17ee7aebd17ad12
http://code.qt.io/cgit/qt/qtdeclarative.git/commit/src/qmltest/quicktest.cpp?id=215c0145be7119a0e885ff95a17ee7aebd17ad12

tests/Render/tst_render.qml
@@ -1,70 +1,80 @@
 import QtQuick 2.0
 import QtTest 1.0
+import QtQuick.Window 2.0
 import Qt.labs.folderlistmodel 2.1
 
 Row {
   id: main
   property int total: 0
   property int current: 0
+  property var testcases: ({})
+  property var queue: 0
+  Repeater {
+    model: FolderListModel {
+      id: dirs
+      folder: "./"
+      showDirs: true
+      showFiles: false
+      sortField: FolderListModel.Name
+      onCountChanged: queue = count
+    }
+    delegate: Item {
+      FolderListModel {
+        nameFilters: ["*.qml"]
+        folder: model.filePath
+        showDirs: false
+        showFiles: true
+        sortField: FolderListModel.Name
+        onCountChanged: {
+          for (var i = 0; i < count; i++) {
+            testcases[get(i, "filePath")] = {
+              dirName: model.fileName,
+              fileName: get(i, "fileName"),
+              async: model.fileName.indexOf("Async") !== -1,
+              dirPath: model.filePath,
+              filePath: get(i, "filePath")
+            };
+          }
+          queue--;
+        }
+      }
+    }
+  }
+  TestCase {
+    name: "RenderTests"
+    when: dirs.count > 0 && queue === 0
+
+    function test_render() {
+      var keys = Object.keys(testcases);
+      compare(
+        Screen.devicePixelRatio, 1,
+        "This testcase requires QT_SCREEN_SCALE_FACTORS=1 QT_AUTO_SCREEN_SCALE_FACTOR=0"
+      );
+
+      for (var i = 0; i < keys.length; i++) {
+        var test = testcases[keys[i]];
+        var name = test.dirName + "." + test.fileName.replace(/\.qml$/, '');
+        image.source = test.filePath.replace(/\.qml$/, ".png");
+        waitForRendering(image);
+        window.loaded = false;
+        loader.source = test.filePath;
+        waitForRendering(loader);
+        if (test.async) {
+          tryCompare(window, "loaded", true);
+        }
+        var actual = grabImage(actualRect);
+        var expect = grabImage(expectRect);
+        loader.source = "";
+        image.source = "";
+        verify(actual.equals(expect), name);
+      }
+    }
+  }
   Item {
     id: window
     property bool loaded: false
     function onTestLoad(params) {
       loaded = true;
-    }
-  }
-  Timer { // needed only to prevent the binding loop warning
-    id: currentWrap
-    running: false
-    interval: 1
-    repeat: false
-    onTriggered: main.current++
-  }
-  Repeater {
-    model: FolderListModel {
-      folder: "./"
-      showDirs: true
-      showFiles: false
-    }
-    delegate: Item {
-      id: group
-      property string name: model.fileName
-      property bool async: model.fileName.indexOf("Async") !== -1
-      property url path: model.filePath
-      Repeater {
-        width: 100
-        height: 100
-        model: FolderListModel {
-          nameFilters: ["*.qml"]
-          folder: group.path
-          showDirs: false
-        }
-        delegate: TestCase {
-          name: group.name + "." + model.fileName
-          anchors.fill: parent
-
-          property int number: -1
-          Component.onCompleted: number = main.total++
-          when: number == main.current
-          onCompletedChanged: if (completed) currentWrap.start()
-
-          function test_render() {
-            image.source = model.filePath.replace(/\.qml$/, ".png");
-            waitForRendering(image);
-            window.loaded = false;
-            loader.source = model.filePath;
-            waitForRendering(loader);
-            if (group.async) {
-              tryCompare(window, "loaded", true);
-            }
-            var actual = grabImage(actualRect);
-            var expect = grabImage(expectRect);
-            loader.source = "";
-            image.source = "";
-            verify(actual.equals(expect));
-          }
-        }
-      }
     }
   }
   Rectangle {