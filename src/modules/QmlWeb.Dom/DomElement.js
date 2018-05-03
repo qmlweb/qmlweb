60759ee9f0ea9d83bce8d429ed5ea151d9d1d12d
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-05-03 16:01:39 +0300

    QmlWeb.Dom/DomElement: support setting attrs and style
    Example:
    
    ```
    import QtQuick 2.0
    import QmlWeb.Dom 1.0
    
    DomElement {
      width: 100
      height: 200
      attrs.id: 'myElement'
      style.background: 'green'
    }
    ```
    
    This works only on DomElement elements (from QmlWeb.Dom) module which
    contains DOM-specific components and is not interoperable with Qt QML.
    
    It is, however, possible to at least partially mock QmlWeb.Dom behavior
    with QtQuick QML, but that's not in scope.

--- src/modules/QmlWeb.Dom/DomElement.js
+++ src/modules/QmlWeb.Dom/DomElement.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-undef
 class QmlWeb_Dom_DomElement extends QtQuick_Item {
   static properties = {
+    attrs: { type: "var", initialValue: {} },
+    style: { type: "var", initialValue: {} },
     tagName: { type: "string", initialValue: "div" }
   };
 
@@ -8,6 +10,13 @@ class QmlWeb_Dom_DomElement extends QtQuick_Item {
     meta.tagName = meta.object.tagName;
     super(meta);
 
-    // TODO: support properties, styles, perhaps changing the tagName
+    for (const key in meta.object.attrs) {
+      if (!meta.object.attrs.hasOwnProperty(key)) continue;
+      this.dom[key] = meta.object.attrs[key];
+    }
+    for (const key in meta.object.style) {
+      if (!meta.object.style.hasOwnProperty(key)) continue;
+      this.dom.style[key] = meta.object.style[key];
+    }
   }
 }
