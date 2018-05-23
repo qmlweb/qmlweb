85fa0f7bb161551c2aa4a9f05ecb7bc1db94e812
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-05-24 01:54:20 +0300

    Revert "tests: failing tests for properties and signals naming conflicts (#440)"
    This reverts commit e42b4e1d0dee6df23155291e6af2a6eb5b158dce.
    
    Those tests are failing on Qt QML, that is not supported.

--- tests/failingTests.js
+++ tests/failingTests.js
@@ -11,10 +11,6 @@ window.failingTests = {
     imports: [
       "Qmldir singleton"
     ],
-    properties: [
-      "Property and signal can have the same names",
-      "Property and signal can have the same names(reverse order)"
-    ],
     scope: [
       "object id should override same-named property of base object"
     ]
