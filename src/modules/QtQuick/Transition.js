bc059b861e0027461096ed0d5ba05c5cc046e521
Michael <michael@unetresgrossebite.com>
2022-04-06 12:59:54 +0200

    * adds running property on Transition * initialize target on Transition animations

--- src/modules/QtQuick/Transition.js
+++ src/modules/QtQuick/Transition.js
@@ -4,14 +4,35 @@ class QtQuick_Transition extends QtQml_QtObject {
     animations: "list",
     from: { type: "string", initialValue: "*" },
     to: { type: "string", initialValue: "*" },
-    reversible: "bool"
+    reversible: "bool",
+    running: "bool"
   };
   static defaultProperty = "animations";
 
   constructor(meta) {
     super(meta);
-
     this.$item = this.$parent;
+    this.animationsChanged.connect(this, this.$updateAnimations);
+  }
+  $updateAnimations() {
+    const flags = QmlWeb.Signal.UniqueConnection;
+    for (let i = 0; i < this.animations.length; i++) {
+      const animation = this.animations[i];
+      animation.runningChanged.connect(this, this.$updateRunning, flags);
+      if (animation.target === this || !animation.target) {
+        animation.$properties.target.set(this.$parent, QMLProperty.ReasonInit);
+      }
+    }
+  }
+  $updateRunning() {
+    let running = false;
+    for (let i = 0; i < this.animations.length; i++) {
+      if (this.animations[i].running) {
+        running = true;
+        break;
+      }
+    }
+    if (this.running !== running) this.running = running;
   }
   $start(actions) {
     for (let i = 0; i < this.animations.length; i++) {
