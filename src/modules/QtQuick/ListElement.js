86eadaab55471808ce02810b2abe8a1060ec1813
Michael <michael@unetresgrossebite.com>
2022-04-10 13:56:37 +0200

    Re-write ListElement using $setCustomData
    Merging 2443508

--- src/modules/QtQuick/ListElement.js
+++ src/modules/QtQuick/ListElement.js
@@ -1,13 +1,8 @@
 // eslint-disable-next-line no-undef
 class QtQuick_ListElement extends QtQml_QtObject {
-  constructor(meta) {
-    super(meta);
-
-    for (const i in meta.object) {
-      if (i[0] !== "$") {
-        QmlWeb.createProperty("variant", this, i);
-      }
-    }
-    QmlWeb.applyProperties(meta.object, this, this, this.$context);
+  $setCustomData(propName, value) {
+    QmlWeb.createProperty("variant", this, propName, {
+      initialValue: value
+    });
   }
 }
